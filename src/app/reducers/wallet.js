304ad0dc9e75cbe411531a14b8ddc56bb4ac2b6f
Dylan Seago <dylan.seago@gmail.com>
2017-12-20 16:57:43 -0500

    Update address in redux

--- src/app/reducers/wallet.js
+++ src/app/reducers/wallet.js
@@ -2,21 +2,24 @@ const initialState = {
   opened: 0
 }
 
-export default (state = initialState, action) => {
-  switch (action.type) {
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
   case 'RESET_ALL':
     return initialState
   case 'SET_WALLET':
     return {
       ...state,
-      type: action.payload.type,
-      address: action.payload.address,
-      data: action.payload.data
+      type: payload.type,
+      address: payload.address,
+      data: payload.data
     }
   case 'WALLET_OPENED':
+    const { address } = payload.wallet
     return {
       ...state,
-      address: action.payload.address,
+      address: typeof state.address !== 'undefined'
+        ? (Array.isArray(state.address) ? [...state.address, address] : [state.address, address])
+        : address,
       opened: state.opened + 1
     }
   default:
