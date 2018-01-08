5a2516d0551ba32082eeeac2f4b5d64e90e84144
Dylan Seago <dylan.seago@gmail.com>
2018-01-08 15:27:20 -0500

    Add isMobile to reduced mediaQueries state

--- src/app/reducers/mediaQueries.js
+++ src/app/reducers/mediaQueries.js
@@ -1,9 +1,13 @@
+import { mediaBreakpointDown } from 'Utilities/breakpoints'
+
 const initialState = {}
 
-export default (state = initialState, action) => {
-  switch (action.type) {
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
   case 'SET_MEDIA_QUERIES':
-    return Object.assign({}, state, action.payload)
+    return Object.assign({}, state, payload, {
+      isMobile: mediaBreakpointDown(payload, 'xs')
+    })
   default:
     return state
   }
