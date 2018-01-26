37fc7ef1e70a32f80437f6ffa39f003de753fbae
Dylan Seago <dylan.seago@gmail.com>
2018-01-26 02:02:07 -0500

    Support segwit ypubs in Bitcore service

--- src/services/Bitcore.js
+++ src/services/Bitcore.js
@@ -1,4 +1,5 @@
 import { networks } from 'bitcoinjs-lib-zcash'
+import b58 from 'bs58check'
 
 import { WorkerDiscovery, BitcoreBlockchain } from 'hd-wallet'
 
@@ -25,14 +26,31 @@ const assetToNetwork = {
   LTC: networks.litecoin
 }
 
+/** Takes ypub and turns into xpub
+  * Source: https://github.com/bitcoinjs/bitcoinjs-lib/issues/966
+  */
+function ypubToXpub(ypub) {
+  let data = b58.decode(ypub)
+  data = data.slice(4)
+  data = Buffer.concat([Buffer.from('0488b21e','hex'), data])
+  return b58.encode(data)
+}
+
 function discover(asset, xpub, onUpdate) {
   return new Promise((resolve, reject) => {
     const network = assetToNetwork[asset]
     if (!network) {
       return reject(new Error(`No network defined for asset ${asset}`))
     }
-    const process = discovery.discoverAccount(null, xpub, network);
-    process.stream.values.attach(onUpdate);
+    let segwit = 'off'
+    if (xpub.startsWith('ypub')) {
+      segwit = 'p2sh'
+      xpub = ypubToXpub(xpub)
+    }
+    const process = discovery.discoverAccount(null, xpub, network, segwit);
+    if (onUpdate) {
+      process.stream.values.attach(onUpdate);
+    }
     process.ending.then(resolve);
   })
 }
