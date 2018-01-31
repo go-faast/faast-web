2d92ce14b05d0b1d16481b6d7a2189956d3aa562
Dylan Seago <dylan.seago@gmail.com>
2018-01-30 19:15:38 -0500

    Fix WalletOpened and WalletClosed routes

--- src/app/components/WalletClosed/index.jsx
+++ src/app/components/WalletClosed/index.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Route, Redirect } from 'react-router-dom'
-import { getCurrentWallet } from 'Selectors'
+import { getCurrentPortfolio } from 'Selectors'
 
-const WalletClosed = ({ wallet, path, component: Component }) => (
+const WalletClosed = ({ portfolio, path, component: Component }) => (
   <Route path={path} render={props => (
-    (wallet && wallet.id) ? (
+    (portfolio && portfolio.wallets.length > 0) ? (
       <Redirect to='/balances' />
     ) : (
       <Component {...props} />
@@ -15,13 +15,13 @@ const WalletClosed = ({ wallet, path, component: Component }) => (
 )
 
 WalletClosed.propTypes = {
-  wallet: PropTypes.object.isRequired,
+  portfolio: PropTypes.object.isRequired,
   path: PropTypes.string.isRequired,
   component: PropTypes.func.isRequired
 }
 
 const mapStateToProps = (state) => ({
-  wallet: getCurrentWallet(state)
+  portfolio: getCurrentPortfolio(state)
 })
 
 export default connect(mapStateToProps)(WalletClosed)
--- src/app/components/WalletOpened/index.jsx
+++ src/app/components/WalletOpened/index.jsx
@@ -2,37 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Route, Redirect } from 'react-router-dom'
-import { getCurrentPortfolio, getCurrentWallet } from 'Selectors'
+import { getCurrentPortfolio } from 'Selectors'
 
-const WalletOpened = ({ wallet, portfolio, path, component: Component }) => (
-  <Route path={path} render={props => {
-    if (wallet && wallet.id) {
-      if (path === '/balances' || portfolio.list.length) {
-        return (
-          <Component {...props} />
-        )
-      } else {
-        return (
-          <Redirect to='/balances' />
-        )
-      }
-    } else {
-      return (
-        <Redirect to='/' />
-      )
-    }
-  }} />
+const WalletOpened = ({ portfolio, path, component: Component }) => (
+  <Route path={path} render={props => (
+    (portfolio && portfolio.wallets.length > 0) ? (
+      <Component {...props} />
+    ) : (
+      <Redirect to='/' />
+    )
+  )} />
 )
 
 WalletOpened.propTypes = {
-  wallet: PropTypes.object.isRequired,
   portfolio: PropTypes.object.isRequired,
   path: PropTypes.string.isRequired,
   component: PropTypes.func.isRequired
 }
 
 const mapStateToProps = (state) => ({
-  wallet: getCurrentWallet(state),
   portfolio: getCurrentPortfolio(state)
 })
 
