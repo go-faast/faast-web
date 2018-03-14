bec894e6d97ea173c75ea34a9ff8dca35883bfca
Dylan Seago <dylan.seago@gmail.com>
2018-03-14 18:22:34 -0400

    Overhaul webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,86 @@
-var webpack = require('webpack')
-var path = require('path')
-var OpenBrowserPlugin = require('open-browser-webpack-plugin')
-var UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const HtmlPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const FaviconPlugin = require('favicons-webpack-plugin')
+const CleanPlugin = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
-var WEBPACK_PORT = process.env.WEBPACK_PORT
+const pkg = require('./package.json')
 
-var cssLoaders = ({ sourceMap = true, modules = true } = {}) => [{
-  loader: 'style-loader'
-}, {
-  loader: 'css-loader', // translates CSS into CommonJS modules
-  options: {
-    sourceMap,
-    modules,
-    importLoaders: 2,
-    localIdentName: '[name]__[local]__[hash:base64:5]'
-  }
-}, {
-  loader: 'postcss-loader', // Run post css actions
-  options: {
-    sourceMap,
-    plugins: function () { // post css plugins, can be exported to postcss.config.js
-      return [
+const DEFAULT_NODE_ENV = 'production'
+let NODE_ENV = process.env.NODE_ENV
+if (!NODE_ENV) {
+  console.log(`NODE_ENV not specified, using ${DEFAULT_NODE_ENV}`)
+  NODE_ENV = DEFAULT_NODE_ENV
+}
+const isDev = NODE_ENV === 'development'
+
+const isMocking = Boolean(process.env.MOCK)
+
+const projectRoot = path.resolve(__dirname)
+const dist = path.join(projectRoot, 'dist')
+const src = path.join(projectRoot, 'src')
+const res = path.join(projectRoot, 'res')
+const test = path.join(projectRoot, 'test')
+const nodeModules = path.join(projectRoot, 'node_modules')
+
+const cssLoader = ({ sourceMap = true, modules = true } = {}) => ExtractTextPlugin.extract({
+  fallback: 'style-loader',
+  use: [{
+    loader: 'css-loader', // translates CSS into CommonJS modules
+    options: {
+      sourceMap,
+      modules,
+      minimize: !isDev,
+      importLoaders: 2,
+      localIdentName: isDev ? '[name]__[local]__[hash:base64:5]' : '[hash:base64]'
+    }
+  }, {
+    loader: 'postcss-loader', // Run post css actions
+    options: {
+      sourceMap,
+      plugins: () => [ // post css plugins, can be exported to postcss.config.js
         require('precss'),
         require('autoprefixer')
-      ];
+      ]
     }
-  }
-}, {
-  loader: 'sass-loader', // compiles SASS to CSS
-  options: { 
-    includePaths: [
-      path.resolve(__dirname, 'node_modules'),
-    ],
-    sourceMap,
-    // Inject env as sass variable
-    data: `$env: ${process.env.NODE_ENV};`
-  }
-}]
+  }, {
+    loader: 'sass-loader', // compiles SASS to CSS
+    options: { 
+      includePaths: [nodeModules],
+      sourceMap,
+      data: `$env: ${NODE_ENV};` // Inject env as sass variable
+    }
+  }]
+})
 
-var config = {
-  entry: path.join(__dirname, 'src', 'index.jsx'),
+const fileOutputDir = 'file/'
+const fileLoader = () => ({
+  loader: 'file-loader',
+  options: {
+    outputPath: fileOutputDir,
+  }
+})
+
+const assetOutputDir = 'asset/'
+const assetLoader = (subDir) => ({
+  loader: 'file-loader',
+  options: {
+    context: res,
+    outputPath: assetOutputDir,
+    name: isDev ? '[path][name].[ext]' : `${subDir}/[hash].[ext]`
+  }
+})
+
+let config = {
+  context: projectRoot,
+  entry: path.join(src, 'index.jsx'),
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'portfolio.bundle.js'
+    path: dist,
+    filename: 'portfolio.[hash:10].js'
   },
   node: {
     fs: 'empty',
@@ -63,28 +101,32 @@ var config = {
       oneOf: [
         {
           resourceQuery: /nsm/,
-          use: cssLoaders({ modules: false })
+          use: cssLoader({ modules: false })
         },
         {
-          use: cssLoaders(),
+          use: cssLoader(),
         }
       ]
     }, {
       resourceQuery: /file/,
-      loader: 'file-loader',
-      options: {
-        outputPath: 'file/',
-      }
+      use: fileLoader()
+    }, {
+      test: /\.(png|jpe?g|gif|svg|ico)(\?.*)?$/,
+      use: assetLoader('img')
+    }, {
+      test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+      use: assetLoader('font')
     }]
   },
   resolve: {
     alias: {
-      Src: path.resolve(__dirname, 'src'),
-      Test: path.resolve(__dirname, 'test'),
+      Src: src,
+      Res: res,
+      Test: test,
       Mock: 'Test/mock',
       Config: 'Src/config',
       Utilities: 'Src/utilities',
-      Services: process.env.MOCK ? 'Mock/services' : 'Src/services',
+      Services: isMocking ? 'Mock/services' : 'Src/services',
       App: 'Src/app',
       Components: 'App/components',
       Styles: 'App/styles',
@@ -93,40 +135,75 @@ var config = {
       Selectors: 'App/selectors'
     },
     extensions: ['.js', '.jsx', '.json', '.scss', '.css']
-  }
+  },
+  plugins: [
+    new CleanPlugin(['*.*', assetOutputDir, fileOutputDir], { root: dist }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
+    }),
+    new HtmlPlugin({
+      template: path.join(src, 'index.html'),
+      filename: 'index.html',
+      title: pkg.productName,
+      minify: isDev ? false : {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+        html5: true,
+        minifyCSS: true,
+        removeComments: true,
+        removeEmptyAttributes: true,
+      }
+    }),
+    new ExtractTextPlugin({
+      filename: 'style.[contenthash:10].css',
+      allChunks: true,
+      ignoreOrder: true,
+      disable: isDev
+    }),
+    new FaviconPlugin({
+      logo: path.join(res, 'img', 'faast-logo.png'),
+      prefix: 'favicon-[hash:10]/',
+      title: pkg.productName,
+      description: pkg.description,
+      background: '#181818',
+      emitStats: false,
+      cache: true,
+      inject: true
+    }),
+    new CopyPlugin([{ from: path.join(res, 'vendor'), to: path.join(dist, 'vendor') }])
+  ]
 }
 
-if (process.env.NODE_ENV === 'production') {
-  config.devtool = 'source-map'
-  config.plugins = [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    new UglifyJsPlugin({
-      sourceMap: true,
-      uglifyOptions: {
-        mangle: {
-          reserved: ['BigInteger', 'ECPair', 'Point']
+if (!isDev) {
+  config = merge(config, {
+    devtool: 'source-map',
+    plugins: [
+      new UglifyJsPlugin({
+        sourceMap: true,
+        uglifyOptions: {
+          mangle: {
+            reserved: ['BigInteger', 'ECPair', 'Point']
+          }
         }
-      }
-    })
-  ]
+      })
+    ]
+  })
 } else {
-  config.devtool = 'eval-source-map'
-  config.devServer = {
-    compress: true,
-    historyApiFallback: true,
-    inline: true,
-    headers: { 'Access-Control-Allow-Origin': '*' }
-  }
-  config.output.publicPath = `http://localhost:${WEBPACK_PORT}/`
-  config.plugins = [
-    new OpenBrowserPlugin({ url: 'http://localhost:5000/?dev=true' }),
-    new webpack.DefinePlugin({
-      SITE_URL: JSON.stringify(process.env.SITE_URL),
-      API_URL: JSON.stringify(process.env.API_URL)
-    })
-  ]
+  config = merge(config, {
+    devtool: 'eval-source-map',
+    devServer: {
+      compress: true,
+      historyApiFallback: true,
+      inline: true,
+      headers: { 'Access-Control-Allow-Origin': '*' }
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        SITE_URL: JSON.stringify(process.env.SITE_URL),
+        API_URL: JSON.stringify(process.env.API_URL)
+      })
+    ]
+  })
 }
 
 module.exports = config
