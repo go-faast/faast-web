b37a1c4c949705648dfc5b8516411508afe42cdf
Dylan Seago <dylan.seago@gmail.com>
2018-03-16 17:43:09 -0400

    Fix error message handling in updateWalletBalances

--- src/app/actions/wallet.js
+++ src/app/actions/wallet.js
@@ -88,7 +88,10 @@ export const updateWalletBalances = (walletId) => (dispatch) => Promise.resolve(
   })
   .catch((e) => {
     log.error(`Failed to update balances for wallet ${walletId}:`, e)
-    let message = e.message
+    let message = e.message || e
+    if (typeof message !== 'string') { 
+      message = 'Unknown error occured while updating wallet balances'
+    }
     if (message.includes('Failed to fetch')) {
       message = 'Error loading wallet balances'
     }
