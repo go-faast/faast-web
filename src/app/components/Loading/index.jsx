38010554251128c403913d2f7a6e7dc0031bfafd
Dylan Seago <dylan.seago@gmail.com>
2018-03-18 13:40:27 -0400

    Change Loading component to use local image

--- src/app/components/Loading/index.jsx
+++ src/app/components/Loading/index.jsx
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types'
 import classNames from 'class-names'
 import styles from './style'
 
+import faastLogo from 'Img/faast-logo.png'
+
 const Loading = ({ error, center }) => (
   <div className={classNames(styles.loading, { 'm-auto': center })}>
-    <img src='https://faa.st/img/faast-transparent-bad.png' height='50' />
+    <img src={faastLogo} height='50' />
     <span className={styles.spinnerContainer}>
       {error
         ? (<span className='text-danger'>{error}</span>)
