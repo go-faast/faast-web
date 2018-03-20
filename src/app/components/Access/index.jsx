8f7cd02a1ff0d9326588e3b81a9864d1411ef7be
Dylan Seago <dylan.seago@gmail.com>
2018-03-20 17:16:47 -0400

    Refactor Access page

--- src/app/components/Access/index.jsx
+++ src/app/components/Access/index.jsx
@@ -13,8 +13,6 @@ import Blockstack from 'Components/Blockstack'
 
 import { isDefaultPortfolioEmpty } from 'Selectors'
 
-import styles from './style'
-
 const TileRow = ({ children }) => (<div className='my-3'><Row className='gutter-4 justify-content-center'>{children}</Row></div>)
 const TileCol = ({ children }) => (<Col xs='9' sm='6' md='4' lg='3'>{children}</Col>)
 
@@ -25,37 +23,33 @@ const Access = ({ disablePortfolioAccess }) => (
         <Button color='faast' tag={Link} to='/balances' disabled={disablePortfolioAccess}>portfolio</Button>
       </Col>
     </Row>
-    <h3 className={styles.title}>Select your wallet</h3>
-    <h4 className={styles.walletRowHeading}>Hardware Wallets</h4>
-    <h6 className={styles.walletRowSubheading}>Recommended</h6>
-    <TileRow>
-      <TileCol><HardwareWallet type='trezor' /></TileCol>
-      <TileCol><HardwareWallet type='ledger' /></TileCol>
-    </TileRow>
-    <h4 className={styles.walletRowHeading}>Software Wallets</h4>
-    <TileRow>
-      <TileCol><Web3Wallet type='metamask' /></TileCol>
-      <TileCol><Web3Wallet type='mist' /></TileCol>
-      <TileCol><Web3Wallet type='parity' /></TileCol>
-      <TileCol><Blockstack /></TileCol>
-    </TileRow>
-    <h4 className={styles.walletRowHeading}>Manual</h4>
-    <TileRow>
-      <TileCol><Keystore /></TileCol>
-      <TileCol><CreateWallet /></TileCol>
-    </TileRow>
-    <div className={`row ${styles.bottomContainer}`}>
-      <div className='col'>
-        <div className={styles.openSourceContainer}>
-          <a className={styles.openSourceLink} href='https://github.com/go-faast/faast-portfolio' target='_blank' rel='noopener noreferrer'>
-            <div className={styles.openSource}>
-              <i className='fa fa-github fa-2x' />
-              <span className={styles.openSourceText}>open source and secure</span>
-              <i className='fa fa-lock fa-2x' />
-            </div>
-          </a>
-        </div>
-      </div>
+    <div className='text-center'>
+      <h3 className='mt-5'>Select your wallet</h3>
+      <h4 className='text-grey mt-4'>Hardware Wallets</h4>
+      <h6 className='text-primary'>Recommended</h6>
+      <TileRow>
+        <TileCol><HardwareWallet type='trezor' /></TileCol>
+        <TileCol><HardwareWallet type='ledger' /></TileCol>
+      </TileRow>
+      <h4 className='text-grey'>Software Wallets</h4>
+      <TileRow>
+        <TileCol><Web3Wallet type='metamask' /></TileCol>
+        <TileCol><Web3Wallet type='mist' /></TileCol>
+        <TileCol><Web3Wallet type='parity' /></TileCol>
+        <TileCol><Blockstack /></TileCol>
+      </TileRow>
+      <h4 className='text-grey'>Manual</h4>
+      <TileRow>
+        <TileCol><Keystore /></TileCol>
+        <TileCol><CreateWallet /></TileCol>
+      </TileRow>
+      <Button style={{ marginTop: '100px' }} color='ultra-dark'
+        tag='a' href='https://github.com/go-faast/faast-portfolio'
+        target='_blank' rel='noopener noreferrer'>
+        <i className='fa fa-github fa-2x align-middle'/>
+        <span className='px-3'>open source and secure</span>
+        <i className='fa fa-lock fa-2x align-middle'/>
+      </Button>
     </div>
   </Layout>
 )
