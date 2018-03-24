8e166a657c9a5af007e3a70aa6ff4874b93cb046
Dylan Seago <dylan.seago@gmail.com>
2018-03-23 23:40:31 -0400

    Start with responsive mobile navbar

--- src/app/components/AppNavbar.jsx
+++ src/app/components/AppNavbar.jsx
@@ -3,7 +3,7 @@ import {
   Container,
   Collapse,
   Navbar,
-  NavbarToggler,
+//  NavbarToggler,
   NavbarBrand,
   Nav,
   NavItem,
@@ -14,7 +14,7 @@ import { Link, NavLink as RouterNavLink } from 'react-router-dom'
 import Icon from 'Components/Icon'
 import FaastLogo from 'Img/faast-logo.png'
 
-export default class AppNavbar extends React.Component {
+class AppNavbar extends React.Component {
   constructor(props) {
     super(props);
 
@@ -29,30 +29,50 @@ export default class AppNavbar extends React.Component {
     });
   }
   render() {
-    const { toggle } = this
-    const { isOpen } = this.state
+    //const { toggle } = this
+    //const { isOpen } = this.state
     const { children, ...props } = this.props
     return (
-      <Navbar color='ultra-dark' dark expand='md' fixed='top' {...props}>
+      <Navbar color='ultra-dark' dark fixed='top' {...props}>
         <Container>
-          <NavbarBrand tag={Link} to='/'><Icon src={FaastLogo} height='1.5rem' width='1.5rem' inline className='ml-3_4r mr-3_2r'/>Faast Portfolio <sup className='beta-tag'>beta</sup></NavbarBrand>
-          <NavbarToggler onClick={toggle} />
-          <Collapse isOpen={isOpen} navbar>
+          <NavbarBrand tag={Link} to='/' className='expand-only'>
+            <Icon src={FaastLogo} height='1.5rem' width='1.5rem' inline className='ml-3_4r mr-3_2r'/>Faast Portfolio <sup className='beta-tag'>beta</sup>
+          </NavbarBrand>
+          <NavbarBrand tag={Link} to='/' className='collapse-only'>
+            <i className='fa fa-arrow-left ml-3_4r mr-3_2r'/>Page
+          </NavbarBrand>
+          {/*<NavbarToggler onClick={toggle} />*/}
+          {/*<Collapse isOpen={isOpen} navbar>*/}
             <Nav className='ml-auto' navbar>
-              <NavItem>
-                <NavLink tag={RouterNavLink} to='/balances/'><i className='fa fa-pie-chart'/> Dashboard</NavLink>
+              <NavItem className='expand-only'>
+                <NavLink tag={RouterNavLink} to='/balances/'>
+                  <i className='nav-link-icon fa fa-pie-chart'/>
+                  <span className='nav-link-label'>Dashboard</span>
+                </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={RouterNavLink} to='/connect/'><i className='fa fa-plus'/> add wallet</NavLink>
+                <NavLink tag={RouterNavLink} to='/connect/'>
+                  <i className='nav-link-icon fa fa-plus'/>
+                  <span className='nav-link-label expand-only'>Add wallet</span>
+                </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={RouterNavLink} to='/modify/'><i className='fa fa-exchange'/> swap</NavLink>
+                <NavLink tag={RouterNavLink} to='/modify/'>
+                  <i className='nav-link-icon fa fa-exchange'/>
+                  <span className='nav-link-label'>Swap</span>
+                </NavLink>
               </NavItem>
             </Nav>
-          </Collapse>
+          {/*</Collapse>*/}
         </Container>
         {children}
       </Navbar>
     );
   }
 }
+
+AppNavbar.defaultProps = {
+  expand: 'md'
+}
+
+export default AppNavbar
