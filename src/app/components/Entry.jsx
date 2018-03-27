6c64d1d3f6e792712d2b2883ed22e803aa4ac0e9
Dylan Seago <dylan.seago@gmail.com>
2018-03-26 23:37:55 -0400

    Move stylesheet imports to Entry to enable css HMR

--- src/app/components/Entry.jsx
+++ src/app/components/Entry.jsx
@@ -5,6 +5,12 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Route } from 'react-router-dom'
 import { hot } from 'react-hot-loader'
+
+// Import stylesheets here so they're noticed by HMR
+import 'react-redux-toastr/src/styles/index.scss?nsm'
+import 'Styles/global?nsm'
+import 'faast-ui'
+
 import { init } from 'Actions/app'
 import { isAppReady, getAppError } from 'Selectors'
 import App from 'Components/App'
