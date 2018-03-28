67175e008ba4407204d5cc0fdd1a806572f03f36
Dylan Seago <dylan.seago@gmail.com>
2018-03-28 12:25:29 -0400

    Add bigNumber propType

--- src/utilities/propTypes.js
+++ src/utilities/propTypes.js
@@ -1,3 +1,15 @@
 import PropTypes from 'prop-types'
 
+import { BigNumber } from 'Utilities/convert'
+
 export const tag = PropTypes.oneOfType([PropTypes.string, PropTypes.func])
+
+export const bigNumber = PropTypes.oneOfType([PropTypes.number, PropTypes.instanceOf(BigNumber)])
+
+export * from 'prop-types'
+
+export default {
+  ...PropTypes,
+  tag,
+  bigNumber
+}
