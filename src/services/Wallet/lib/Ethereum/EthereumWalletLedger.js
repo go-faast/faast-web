ca36c53023c85cb5111689cebcab7b6617868c70
Dylan Seago <dylan.seago@gmail.com>
2018-05-04 14:05:14 -0400

    Fix syntax error caused by rebase

--- src/services/Wallet/lib/Ethereum/EthereumWalletLedger.js
+++ src/services/Wallet/lib/Ethereum/EthereumWalletLedger.js
@@ -45,38 +45,34 @@ export default class EthereumWalletLedger extends EthereumWallet {
     return Promise.resolve().then(() => {
       const { txData } = tx
       let ethJsTx
-      try {
-        ethJsTx = new EthereumjsTx(txData)
-        ethJsTx.raw[6] = Buffer.from([txData.chainId])
-        ethJsTx.raw[7] = 0
-        ethJsTx.raw[8] = 0
-      } catch (e) {
-        return Promise.reject(e)
-      }
+      ethJsTx = new EthereumjsTx(txData)
+      ethJsTx.raw[6] = Buffer.from([txData.chainId])
+      ethJsTx.raw[7] = 0
+      ethJsTx.raw[8] = 0
 
-    return window.faast.hw.ledger.signTransaction_async(this.derivationPath, RLP.encode(ethJsTx.raw))
-      .then((result) => {
-        log.info('ledger wallet signed tx', result)
-        return {
-          signedTxData: this._signedEthJsTxToObject(new EthereumjsTx({
-            ...txData,
-            r: addHexPrefix(result.r),
-            s: addHexPrefix(result.s),
-            v: addHexPrefix(result.v)
-          }))
-        }
-      })
-      .fail((ex) => {
-        if (ex === 'Invalid status 6a80') {
-          throw new Error('Please enable "Contract data" in the Settings of the Ethereum Application and try again')
-        } else if (ex === 'Invalid status 6985') {
-          throw new Error('Transaction was denied')
-        } else if (typeof ex === 'string') {
-          throw new Error(`Error from Ledger Wallet - ${ex}`)
-        } else if (ex.errorCode != null && ex.errorCode === 5) {
-          throw new Error('Transaction timed out, please try again')
-        }
-      })
-  });
+      return window.faast.hw.ledger.signTransaction_async(this.derivationPath, RLP.encode(ethJsTx.raw))
+        .then((result) => {
+          log.info('ledger wallet signed tx', result)
+          return {
+            signedTxData: this._signedEthJsTxToObject(new EthereumjsTx({
+              ...txData,
+              r: addHexPrefix(result.r),
+              s: addHexPrefix(result.s),
+              v: addHexPrefix(result.v)
+            }))
+          }
+        })
+        .fail((ex) => {
+          if (ex === 'Invalid status 6a80') {
+            throw new Error('Please enable "Contract data" in the Settings of the Ethereum Application and try again')
+          } else if (ex === 'Invalid status 6985') {
+            throw new Error('Transaction was denied')
+          } else if (typeof ex === 'string') {
+            throw new Error(`Error from Ledger Wallet - ${ex}`)
+          } else if (ex.errorCode != null && ex.errorCode === 5) {
+            throw new Error('Transaction timed out, please try again')
+          }
+        })
+    })
+  }
 }
-  
