ca5446feb1dc413307f3a1f7581197c07c5ee43e
Dylan Seago <dylan.seago@gmail.com>
2018-07-03 17:01:56 -0400

    Remove window.ledger check in Ledger service

--- src/services/Ledger.js
+++ src/services/Ledger.js
@@ -33,25 +33,19 @@ const serviceConfig = {
   }
 }
 
-let service = null
-
-if (window.ledger) {
-  service = Object.entries(serviceConfig).reduce((apps, [appName, { App, methodNames }]) => {
-    const appPromise = Transport.create().then((comm) => new App(comm))
-    return {
-      ...apps,
-      [appName]: methodNames.reduce((methods, methodName) => ({
-        ...methods,
-        [methodName]: (...args) => appPromise.then((app) => {
-          let fn = app[methodName]
-          if (typeof fn !== 'function') {
-            throw new Error(`Function ${methodName} is not a method of ledger ${appName} app`)
-          }
-          return fn.call(app, ...args)
-        })
-      }), {})
-    }
-  }, {})
-}
-
-export default service
+export default Object.entries(serviceConfig).reduce((apps, [appName, { App, methodNames }]) => {
+  const appPromise = Transport.create().then((comm) => new App(comm))
+  return {
+    ...apps,
+    [appName]: methodNames.reduce((methods, methodName) => ({
+      ...methods,
+      [methodName]: (...args) => appPromise.then((app) => {
+        let fn = app[methodName]
+        if (typeof fn !== 'function') {
+          throw new Error(`Function ${methodName} is not a method of ledger ${appName} app`)
+        }
+        return fn.call(app, ...args)
+      })
+    }), {})
+  }
+}, {})
