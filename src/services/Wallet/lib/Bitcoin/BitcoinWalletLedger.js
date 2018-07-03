070c8b914233de7838db42afe2015039fbc10aff
Dylan Seago <dylan.seago@gmail.com>
2018-07-03 17:04:03 -0400

    Implement Ledger tx signing

--- src/services/Wallet/lib/Bitcoin/BitcoinWalletLedger.js
+++ src/services/Wallet/lib/Bitcoin/BitcoinWalletLedger.js
@@ -2,12 +2,15 @@ import HDKey from 'hdkey'
 
 import config from 'Config'
 import log from 'Utilities/log'
-import { xpubToYpub } from 'Utilities/bitcoin'
+import { xpubToYpub, joinDerivationPath } from 'Utilities/bitcoin'
+import { toMainDenomination, toSmallestDenomination } from 'Utilities/convert'
 import Ledger from 'Services/Ledger'
 
 import BitcoinWallet from './BitcoinWallet'
 
 const typeLabel = config.walletTypes.ledger.name
+
+const DEFAULT_FEE = 10 // Sat/byte
 
 export default class BitcoinWalletLedger extends BitcoinWallet {
 
@@ -44,19 +47,66 @@ export default class BitcoinWalletLedger extends BitcoinWallet {
       })
   }
 
-  _canSendAsset() { return false }
+  _canSendAsset() { return true }
 
-  isReadOnly() { return true }
+  isReadOnly() { return false }
 
-  createTransaction() {
-    throw new Error('Unsupported operation')
+  _createTransaction(toAddress, amount, asset, options = {}) {
+    return this._getDiscoveryResult().then((discoverResult) => {
+      const feeRate = options.feeRate || DEFAULT_FEE
+      const isSegwit = !this.isLegacyAccount()
+      return this.bitcore.buildPaymentTx(discoverResult, toAddress, toSmallestDenomination(amount, asset.decimals), feeRate, isSegwit)
+    })
+    .then((txData) => {
+      return {
+        feeAmount: toMainDenomination(txData.fee, asset.decimals),
+        feeSymbol: 'BTC',
+        txData,
+      }
+    })
   }
 
-  _signTx() {
-    throw new Error('Unsupported operation')
+  /**
+  * Sign a transaction using ledgerjs api
+  */
+  _signTx(tx) {
+    const { txData } = tx
+    return Promise.all(txData.inputUtxos.map((utxo) =>
+      this.bitcore.lookupTransaction(utxo.transactionHash)
+        .then((txInfo) => Ledger.btc.splitTransaction(txInfo.hex, true, false))
+        .then((splitTx) => ({
+          ...utxo,
+          splitTx
+        }))))
+      .then((inputUtxos) => {
+        log.info('inputUtxos', inputUtxos)
+
+        const inputs = []
+        const paths = []
+        inputUtxos.forEach((utxo) => {
+          inputs.push([utxo.splitTx, utxo.index])
+          paths.push(joinDerivationPath(this.derivationPath, utxo.addressPath))
+        })
+
+        const changePath = joinDerivationPath(this.derivationPath, txData.changePath)
+        return Ledger.btc.createPaymentTransactionNew(
+          inputs,
+          paths,
+          changePath,
+          txData.outputScript,
+          undefined, // lockTime, default (0)
+          undefined, // sigHashType, default (all)
+          txData.isSegwit)
+      })
+      .then((signedTxHex) => ({
+        signedTxData: signedTxHex
+      }))
   }
 
-  _sendSignedTx() {
-    throw new Error('Unsupported operation')
+  _validateTxData(txData) {
+    if (txData === null || typeof txData === 'object') {
+      throw new Error(`Invalid ${this.getType()} txData of type ${typeof txData}`)
+    }
+    return txData
   }
 }
