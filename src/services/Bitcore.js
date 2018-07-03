eac0823956d9f561f2746c72722823273aa91289
Dylan Seago <dylan.seago@gmail.com>
2018-07-03 17:02:52 -0400

    Implement buildPaymentTx in Bitcore service

--- src/services/Bitcore.js
+++ src/services/Bitcore.js
@@ -1,5 +1,8 @@
 import { networks } from 'bitcoinjs-lib-zcash'
 import { WorkerDiscovery, BitcoreBlockchain } from 'hd-wallet'
+import { TransactionBuilder } from 'bitcoinjs-lib'
+import BigNumber from 'bignumber.js'
+import { isString } from 'lodash'
 
 import xpubWasmFile from 'hd-wallet/lib/fastxpub/fastxpub.wasm?file'
 
@@ -7,7 +10,7 @@ import XpubWorker from 'hd-wallet/lib/fastxpub/fastxpub?worker'
 import SocketWorker from 'hd-wallet/lib/socketio-worker/inside?worker'
 import DiscoveryWorker from 'hd-wallet/lib/discovery/worker/inside?worker'
 
-import { ypubToXpub } from 'Utilities/bitcoin'
+import { ypubToXpub, estimateTxFee } from 'Utilities/bitcoin'
 
 // setting up workers
 const xpubWorker = new XpubWorker()
@@ -17,150 +20,150 @@ const xpubWasmFilePromise = fetch(xpubWasmFile)
 const socketWorkerFactory = () => new SocketWorker()
 const discoveryWorkerFactory = () => new DiscoveryWorker()
 
-class Bitcore {
-  constructor(networkConfig, bitcoreUrls) {
+/**
+ * Sort the utxos for input selection
+ * 
+ * Each utxo object requires at least 2 fields, value (satoshis) and confirmations.
+ */
+function sortUtxos(utxoList) {
+  const matureList = []
+  const immatureList = []
+  utxoList.forEach((utxo) => {
+    if (utxo.confirmations >= 6) {
+      matureList.push(utxo)
+    } else {
+      immatureList.push(utxo)
+    }
+  })
+  matureList.sort((a, b) => a.value - b.value) // Ascending order by value
+  immatureList.sort((a, b) => b.confirmations - a.confirmations) // Descending order by confirmations
+  return matureList.concat(immatureList)
+}
+
+class Bitcore extends BitcoreBlockchain {
+  constructor(assetSymbol, networkConfig, bitcoreUrls) {
+    super(bitcoreUrls, socketWorkerFactory)
+    this.assetSymbol = assetSymbol
     this.network = networkConfig
-    this.bitcoreUrls = bitcoreUrls
-    this.blockchain = new BitcoreBlockchain(bitcoreUrls, socketWorkerFactory)
-    this.discovery = new WorkerDiscovery(discoveryWorkerFactory, xpubWorker, xpubWasmFilePromise, this.blockchain)
+    this.discovery = new WorkerDiscovery(discoveryWorkerFactory, xpubWorker, xpubWasmFilePromise, this)
   }
 
-  discoverAccount = (xpub, onUpdate) => Promise.resolve()
-    .then(() => {
-      let segwit = 'off'
-      if (xpub.startsWith('ypub')) {
-        segwit = 'p2sh'
-        xpub = ypubToXpub(xpub)
-      }
-      const process = this.discovery.discoverAccount(null, xpub, this.network, segwit)
-      if (onUpdate) {
-        process.stream.values.attach(onUpdate)
-      }
-      return process.ending
-    })
+  /**
+   * Discover the balance, transactions, unused addresses, etc of an xpub.
+   *
+   * @param {String} xpub - The xpub or ypub to discover
+   * @param {Function} [onUpdate] - Callback for partial updates to discover result
+   * @returns {Object}
+   */
+  discoverAccount(xpub, onUpdate) {
+    return Promise.resolve()
+      .then(() => {
+        let segwit = 'off'
+        if (xpub.startsWith('ypub')) {
+          segwit = 'p2sh'
+          xpub = ypubToXpub(xpub)
+        }
+        const process = this.discovery.discoverAccount(null, xpub, this.network, segwit)
+        if (onUpdate) {
+          process.stream.values.attach(onUpdate)
+        }
+        return process.ending.then((result) => ({
+          ...result,
+          utxos: result.utxos.map((utxo) => ({
+            ...utxo,
+            confirmations: utxo.height ? result.lastBlock.height - utxo.height : 0
+          }))
+        }))
+      })
+  }
 
-  getTransaction = (txId) => this.blockchain.lookupTransaction(txId)
+  /**
+  * Build a simple payment transaction.
+  *
+  * @param {(String|Object)} account - An xpub to pass to discoverAccount or the result of calling discoverAccount
+  * @param {String} toAddress - address to send to
+  * @param {Number} amount - amount to send (unit: satoshi)
+  * @param {Number} feeRate - desired fee (unit: satoshi per byte)
+  * @param {Boolean} [isSegwit=true] - True if this is a segwit transaction
+  * @returns {Object}
+  */
+  buildPaymentTx(account, toAddress, amount, feeRate, isSegwit = true) {
+    let discoverResult = account
+    if (isString(account)) {
+      discoverResult = this.discoverAccount(account)
+    }
+    const { utxos, changeIndex, changeAddresses } = discoverResult
+    const changeAddress = changeAddresses[changeIndex]
+    amount = new BigNumber(amount)
+    const sortedUtxos = sortUtxos(utxos)
+
+    /* Select inputs and calculate appropriate fee */
+    let fee = new BigNumber(0) // Total fee is recalculated when adding each input
+    let amountWithFee = amount.plus(fee)
+    let inputUtxos = []
+    let inputTotal = new BigNumber(0)
+    for (let i = 0; i < sortedUtxos.length; i++) {
+      fee = new BigNumber(estimateTxFee(feeRate, inputUtxos.length + 1, 2, isSegwit))
+      amountWithFee = amount.plus(fee)
+      let utxo = sortedUtxos[i]
+      inputTotal = inputTotal.plus(utxo.value)
+      inputUtxos.push(utxo)
+      if (inputTotal.greaterThanOrEqualTo(amountWithFee)) break
+    }
+    if (amountWithFee.greaterThan(inputTotal)) {
+      throw new Error(`You do not have enough UTXOs to send ${amount.times(1e-8)} ${this.assetSymbol} with ${feeRate} sat/byte fee`)
+    }
+
+    /* Build outputs */
+    const change = inputTotal.minus(amountWithFee)
+    const outputs = [{
+      address: toAddress,
+      value: amount.toNumber()
+    }, {
+      address: changeAddress,
+      value: change.toNumber()
+    }]
+    const outputBuilder = new TransactionBuilder(this.network)
+    outputs.forEach(({ value, address }) => outputBuilder.addOutput(address, value))
+    const outputScript = outputBuilder.buildIncomplete().toHex().slice(10, -8) // required by ledgerjs api
+
+    return {
+      inputUtxos,
+      outputs,
+      outputScript,
+      toAddress,
+      amount: amount.toNumber(),
+      fee: fee.toNumber(),
+      inputTotal: inputTotal.toNumber(),
+      change: change.toNumber(),
+      changePath: [1, changeIndex],
+      changeAddress,
+      isSegwit,
+    }
+
+  }
 }
 
 const assetToBitcore = {
-  BTC: new Bitcore(networks.bitcoin, ['https://blockexplorer.com', 'https://bitcore1.trezor.io', 'https://bitcore3.trezor.io']),
-  LTC: new Bitcore(networks.litecoin, ['https://ltc-bitcore3.trezor.io']),
+  BTC: new Bitcore('BTC', networks.bitcoin, ['https://blockexplorer.com', 'https://bitcore1.trezor.io', 'https://bitcore3.trezor.io']),
+  LTC: new Bitcore('LTC', networks.litecoin, ['https://ltc-bitcore3.trezor.io']),
 }
 
-const getBitcoreForAsset = (asset) => Promise.resolve()
-  .then(() => {
-    const bitcore = assetToBitcore[asset]
-    if (!bitcore) {
-      throw new Error(`Asset ${asset} has no Bitcore configuration`)
-    }
-    return bitcore
-  })
-
-const discover = (asset, xpub, onUpdate) => getBitcoreForAsset(asset)
-  .then((bitcore) => bitcore.discoverAccount(xpub, onUpdate))
-
-const getTransaction = (asset, txId) => getBitcoreForAsset(asset)
-  .then((bitcore) => bitcore.getTransaction(txId))
-
-import Ledger from 'Services/Ledger'
-import log from 'Utilities/log'
-import { TransactionBuilder } from 'bitcoinjs-lib'
-import BigNumber from 'bignumber.js'
-const segwit = true
-
-/**
- * This is a function for selecting utxos to build transactions.
- * The transaction object requires at least 2 fields, value (satoshis) and confirmations.
- * This function is based off of qtumjs-lib/src/utils.js#selectTxs
- *
- * @param [transaction] allUtxos
- * @param Number amount (unit: satoshi)
- * @param Number fee (unit: satoshi)
- * @returns [transaction]
- */
-function selectUtxos(allUtxos, amount, fee) {
-  //sort the utxo
-  const matureList = []
-  const immatureList = []
-  for (let i = 0; i < allUtxos.length; i++) {
-    if (allUtxos[i].confirmations >= 6) {
-      matureList[matureList.length] = allUtxos[i]
-    } else {
-      immatureList[immatureList.length] = allUtxos[i]
-    }
+/** Get the Bitcore service for the specified asset */
+function getNetwork (assetSymbol) {
+  const bitcore = assetToBitcore[assetSymbol]
+  if (!bitcore) {
+    throw new Error(`Asset ${assetSymbol} has no Bitcore configuration`)
   }
-  matureList.sort(function(a, b) {return a.value - b.value})
-  immatureList.sort(function(a, b) {return b.confirmations - a.confirmations})
-  allUtxos = matureList.concat(immatureList)
-
-  let value = new BigNumber(amount).plus(fee)
-  let find = []
-  let findTotal = new BigNumber(0)
-  for (let i = 0; i < allUtxos.length; i++) {
-    let tx = allUtxos[i]
-    findTotal = findTotal.plus(tx.value)
-    find[find.length] = tx
-    if (findTotal.greaterThanOrEqualTo(value)) break
-  }
-  if (value.greaterThan(findTotal)) {
-    throw new Error('You do not have enough QTUM to send')
-  }
-  return find
+  return bitcore
 }
 
-
-function signTx(assetSymbol, to, amount, fee, changeAddress, utxoList) {
-  return Promise.all(
-    utxoList.map((utxo) => getTransaction(assetSymbol, utxo.transactionHash)
-      .then((txInfo) => Ledger.btc.splitTransaction(txInfo.hex, segwit)
-        .then((splitTx) => ({
-          ...utxo,
-          txInfo,
-          splitTx
-        })))))
-  .then((utxoList) => {
-    console.log(utxoList)
-    const inputs = []
-    const paths = []
-    let totalInputAmount = new BigNumber(0)
-    utxoList.forEach((utxo) => {
-      inputs.push([utxo.splitTx, utxo.index])
-      paths.push('m/49\'/0\'/0\'')
-      totalInputAmount = totalInputAmount.plus(utxo.value)
-    })
-
-    const amountSat = new BigNumber(amount)
-    const feeSat = new BigNumber(fee)
-    const outputs = new TransactionBuilder(assetToNetwork[assetSymbol])
-    outputs.addOutput(to, amountSat.toNumber())
-    const changeSat = totalInputAmount.minus(amountSat).minus(feeSat)
-    outputs.addOutput(changeAddress, changeSat.toNumber())
-    const outputScript = outputs.buildIncomplete().toHex().slice(10, -8)
-
-    return Ledger.btc.createPaymentTransactionNew(...log.infoInline([inputs, paths, undefined, outputScript, undefined, undefined, segwit]))
-  })
-}
-
-discover('BTC', 'ypub6QqdH2c5z79681aKxZwr1kFUvJdpBHd5uLevM3zN8mod2jxySS8t3M4Pqki3HRRYU5LL6qJUY3ZM6qsr6cDYGxuBjCUAaijXUu1p64QUtzc')
-  .then((result) => {
-    console.log(result)
-    return signTx('BTC', '3HLF6edYCPi7pE29V718N4tRH29fXmNdKw', 200000, 3154, '3Qhu8XEAiFRSsNDmvrTiGwZ5FWvtrN9ngx', [{
-      addressPath: [0, 0],
-      coinbase: false,
-      fee: undefined,
-      height: 529348,
-      index: 1,
-      own: false,
-      transactionHash: 'e7b78f00c393a5f12fb3e6dfb5e51c7afb840d3c8a81b37c5fa73d5d4a0b5e51',
-      tsize: 406,
-      value: 286535,
-      vsize: 406,
-    }])
-  })
-  .then(log.infoInline)
-  .catch(log.errorInline)
+const wrapBitcoreFn = (fnName) => (assetSymbol, ...args) => getNetwork(assetSymbol)
+  .then((bitcore) => bitcore[fnName](...args))
 
 export default {
-  discover,
-  getTransaction
+  getNetwork,
+  discoverAccount: wrapBitcoreFn('discoverAccount'),
+  lookupTransaction: wrapBitcoreFn('lookupTransaction'),
+  buildPaymentTx: wrapBitcoreFn('buildPaymentTx'),
 }
--- src/utilities/bitcoin.js
+++ src/utilities/bitcoin.js
@@ -18,8 +18,76 @@ function b58convert(encoded, newMagicNumber) {
 export const ypubToXpub = (ypub) => b58convert(ypub, magicNumber.xpub)
 export const xpubToYpub = (xpub) => b58convert(xpub, magicNumber.ypub)
 
+/**
+ * Estimate size of transaction a certain number of inputs and outputs.
+ * This function is based off of ledger-wallet-webtool/src/TransactionUtils.js#estimateTransactionSize
+ */
+export function estimateTxSize(inputsCount, outputsCount, handleSegwit) {
+  var maxNoWitness,
+    maxSize,
+    maxWitness,
+    minNoWitness,
+    minSize,
+    minWitness,
+    varintLength;
+  if (inputsCount < 0xfd) {
+    varintLength = 1;
+  } else if (inputsCount < 0xffff) {
+    varintLength = 3;
+  } else {
+    varintLength = 5;
+  }
+  if (handleSegwit) {
+    minNoWitness =
+      varintLength + 4 + 2 + 59 * inputsCount + 1 + 31 * outputsCount + 4;
+    maxNoWitness =
+      varintLength + 4 + 2 + 59 * inputsCount + 1 + 33 * outputsCount + 4;
+    minWitness =
+      varintLength +
+      4 +
+      2 +
+      59 * inputsCount +
+      1 +
+      31 * outputsCount +
+      4 +
+      106 * inputsCount;
+    maxWitness =
+      varintLength +
+      4 +
+      2 +
+      59 * inputsCount +
+      1 +
+      33 * outputsCount +
+      4 +
+      108 * inputsCount;
+    minSize = (minNoWitness * 3 + minWitness) / 4;
+    maxSize = (maxNoWitness * 3 + maxWitness) / 4;
+  } else {
+    minSize = varintLength + 4 + 146 * inputsCount + 1 + 31 * outputsCount + 4;
+    maxSize = varintLength + 4 + 148 * inputsCount + 1 + 33 * outputsCount + 4;
+  }
+  return {
+    min: minSize,
+    max: maxSize
+  };
+}
+
+export function estimateTxFee(satPerByte, inputsCount, outputsCount, handleSegwit) {
+  const { min, max } = estimateTxSize(inputsCount, outputsCount, handleSegwit)
+  const mean = Math.ceil((min + max) / 2)
+  return mean * satPerByte
+}
+
+/** Join a base derivation path string with an integer array subpath */
+export function joinDerivationPath(basePathString, subPath) {
+  return `${basePathString}/${subPath.join('/')}`
+}
+
 export default {
   magicNumber,
   ypubToXpub,
-  xpubToYpub
+  xpubToYpub,
+  estimateTxSize,
+  estimateTxFee,
+  joinDerivationPath,
 }
