83f907cf27c0ccf60c9742569bbd57e7552cacba
Dylan Seago <dylan.seago@gmail.com>
2018-08-01 23:03:14 -0400

    Support multi output tx for ledger instructions

---
+++ src/app/components/ConfirmTransactionModal/LedgerInstructions.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { compose, setDisplayName, setPropTypes } from 'recompose'
+
+import LedgerScreen from './LedgerScreen'
+
+export default compose(
+  setDisplayName('LedgerInstructions'),
+  setPropTypes({
+    appName: PropTypes.string.isRequired,
+    screens: PropTypes.arrayOf(PropTypes.shape({
+      top: PropTypes.node,
+      bottom: PropTypes.node,
+    }))
+  })
+)(({ appName, screens }) => (
+  <div>
+    <p>Please confirm the transaction on your device. You should see the following information on screen.</p>
+
+    <p><small>
+      {'Tip: If you don\'t see the transaction on your device, ensure you\'ve unlocked '
+      + `it using your PIN and have opened the ${appName} app.`}
+    </small></p>
+
+    {screens.map(({ top, bottom }, i) => (
+      <LedgerScreen key={i} top={top} bottom={bottom}/>
+    ))}
+  </div>
+))
