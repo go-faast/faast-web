3f0d1eb103938926670d3dee2a6f2e54a43279f1
Dylan Seago <dylan.seago@gmail.com>
2018-08-16 18:06:01 -0400

    Updates to web3 instruction appearance

--- src/app/components/ConfirmTransactionModal/Web3Instructions.jsx
+++ src/app/components/ConfirmTransactionModal/Web3Instructions.jsx
@@ -1,25 +1,77 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { compose, setDisplayName, setPropTypes } from 'recompose'
+import { Row, Col } from 'reactstrap'
 import classNames from 'class-names'
+import { toNumber } from 'Utilities/convert'
+import { ellipsize } from 'Utilities/display'
 import Units from 'Components/Units'
-import Web3Screen from './Web3Screen'
-import { transfer } from './style'
+import {
+  metaMaskContainer, metaMaskAddressRow, metaMaskArrow,
+  metaMaskTransfer, metaMaskNonce, metaMaskAmountRow,
+  metaMaskFeeRow,
+} from './style'
+import arrowRight from 'Img/metamask-arrow-right.svg'
 
-const Web3Instructions = ({ tx: { outputs: [{ amount, address }], assetSymbol, feeAmount, feeSymbol } }) => (
-    <div>
-      <p>Please confirm the transaction. You should see the following information in your MetaMask.</p>
-      <p><small>
-        {'Tip: If you don\'t see a pop up with your order details, click the MetaMask icon in your browser extension menu as shown'}
-        <a href='https://i.imgur.com/Gqn7JUM.mp4' target='_blank' rel='noopener noreferrer'> here.</a>
-      </small></p>
-      <Web3Screen screens={[
-        { left: (<div className={'font-weight-bold wordBreak'}>{address}</div>), right: (<div className={classNames('mt-2 mb-1 font-size-xs', transfer)}>Transfer</div>) },
-        { left: 'Amount', right: (<span className='font-weight-bold float-right'><Units value={amount} symbol={assetSymbol} precision={null}/></span>) },
-        { left: 'Gas fee', right: (<span className='font-weight-bold float-right'><Units value={feeAmount} symbol={feeSymbol} precision={null}/></span>) }
-      ]}/>
+const ETH_SYMBOL = '♦'
+
+const unitProps = (symbol) => symbol === 'ETH' ? { symbol: ETH_SYMBOL, prefixSymbol: true } : { symbol }
+
+const Web3Instructions = ({ tx: { outputs: [{ amount, address }], assetSymbol, feeAmount, feeSymbol, txData } }) => (
+  <div>
+    <p>Please confirm the transaction. You should see the following information in your MetaMask.</p>
+    <p><small>
+      {'Tip: If you don\'t see a pop up with your order details, click the MetaMask icon in your browser extension menu as shown'}
+      <a href='https://i.imgur.com/Gqn7JUM.mp4' target='_blank' rel='noopener noreferrer'> here.</a>
+    </small></p>
+    <div className={metaMaskContainer}>
+      <Row className={classNames('no-gutters justify-content-center align-items-center', metaMaskAddressRow)}>
+        <Col>
+          My Wallet
+        </Col>
+        <Col xs='auto'>
+          <div className={metaMaskArrow}>
+            <img height="15" width="15" src={arrowRight}/>
+          </div>
+        </Col>
+        <Col className='text-right'>
+          {ellipsize(address.toLowerCase(), 6, 4)}
+        </Col>
+      </Row>
+      <div className={metaMaskAmountRow}>
+        <div>
+          <div className={classNames('font-size-xs', metaMaskTransfer)}>{assetSymbol === 'ETH' ? 'Confirm' : 'Transfer'}</div>
+          <div className={classNames('float-right', metaMaskNonce)}>#{toNumber(txData.nonce)}</div>
+        </div>
+        <h2 className='font-weight-bold my-2'>
+          <Units value={amount} precision={8} {...unitProps(assetSymbol)}/>
+        </h2>
+      </div>
+      <div className={metaMaskFeeRow}>
+        <div>
+          GAS FEE
+          <span className='font-weight-bold float-right'>
+            <Units value={feeAmount} precision={4} {...unitProps(feeSymbol)}/>
+          </span>
+        </div>
+        <div>
+          TOTAL
+          <span className='font-weight-bold float-right'>
+            {assetSymbol === 'ETH' ? (
+              <Units value={amount.plus(feeAmount)} precision={8} {...unitProps(assetSymbol)}/>
+            ) : (
+              <Fragment>
+                <Units value={amount} precision={8} {...unitProps(assetSymbol)}/>
+                {' + '}
+                <Units value={feeAmount} precision={4} {...unitProps(feeSymbol)}/>
+              </Fragment>
+            )}
+          </span>
+        </div>
+      </div>
     </div>
-  )
+  </div>
+)
 
 export default compose(
   setDisplayName('Web3Instructions'),
