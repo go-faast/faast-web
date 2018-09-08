43b74223391e77a14e466ae5b4d139372cb8de12
Dylan Seago <dylan.seago@gmail.com>
2018-09-08 13:38:38 -0400

    Hide sign tx instructions during tx submit

--- src/app/components/SwapSubmitModal/index.jsx
+++ src/app/components/SwapSubmitModal/index.jsx
@@ -68,7 +68,8 @@ class SwapSubmitModal extends Component {
     }
 
     let errorMessage = swundle.error
-    const currentSwap = swundle.swaps.find(({ txSigning, txSending }) => txSigning || txSending)
+    const currentSwap = swundle.swaps.find(({ txSigning, txSending, sendWallet }) =>
+      txSigning || (txSending && sendWallet && !sendWallet.isSignTxSupported))
     const showSubmit = !requiresSigning || startedSigning // True if continue button triggers tx sending, false for signing
     const continueDisabled = showSubmit ? (!readyToSend || startedSending) : (!readyToSign || startedSigning)
     const continueLoading = showSubmit ? startedSending : startedSigning
