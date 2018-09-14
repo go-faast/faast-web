e80c2b8178952f655194e5cf04e132913b657293
dwolfgram <daniel.j.wolfgram@gmail.com>
2018-09-14 17:22:03 -0400

    change reducer imports of actions names

--- src/app/reducers/priceChart.js
+++ src/app/reducers/priceChart.js
@@ -1,8 +1,8 @@
 import { createReducer } from 'redux-act'
 import {
-  priceChartDataLoading,
-  priceChartDataUpdated,
-  priceChartDataError
+  priceChartLoading,
+  priceChartUpdated,
+  priceChartError
 } from 'Actions/priceChart'
 import { createUpserter } from 'Utilities/helpers'
 
@@ -20,7 +20,7 @@ const priceChartInitialState = {
 const upsertAsset = createUpserter('symbol', priceChartInitialState)
 
 export default createReducer({
-  [priceChartDataLoading]: (state, symbol) => upsertAsset(state, { symbol, loading: true }),
-  [priceChartDataUpdated]: (state, { symbol, data }) => upsertAsset(state, { symbol, data, loading: false, lastUpdated: Date.now()  }),
-  [priceChartDataError]: (state, { symbol, error }) => upsertAsset(state, { symbol, error }),
+  [priceChartLoading]: (state, symbol) => upsertAsset(state, { symbol, loading: true }),
+  [priceChartUpdated]: (state, { symbol, data }) => upsertAsset(state, { symbol, data, loading: false, lastUpdated: Date.now()  }),
+  [priceChartError]: (state, { symbol, error }) => upsertAsset(state, { symbol, error }),
 }, initialState)
