f1c4a61d727772ae4f26dc250f9e7a4a7ab8eb33
Dylan Seago <dylan.seago@gmail.com>
2018-10-22 14:55:03 -0400

    Add connection instructions for bcash and ltc

--- src/app/components/HardwareWalletModal/ConnectionInstructions.jsx
+++ src/app/components/HardwareWalletModal/ConnectionInstructions.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'class-names'
 
-const trezorInstructions = () => [
+const defaultTrezorInstructions = (assetName) => [
   {
     icon: 'fa-usb',
     text: 'Connect your TREZOR to begin'
@@ -11,9 +11,13 @@ const trezorInstructions = () => [
     icon: 'fa-unlock',
     text: 'If required, enter your pin or password to unlock the TREZOR'
   },
+  {
+    icon: 'fa-th-list',
+    text: `When shown a list of ${assetName} accounts, select the one you want to use`,
+  }
 ]
 
-const ledgerInstructions = (assetName) => [
+const defaultLedgerInstructions = (assetName) => [
   {
     icon: 'fa-usb',
     text: 'Connect your Ledger device to begin'
@@ -28,10 +32,10 @@ const ledgerInstructions = (assetName) => [
   },
 ]
 
-const instructions = {
+const assetInstructionOverrides = {
   ledger: {
     ETH: [
-      ...ledgerInstructions('Ethereum'),
+      ...defaultLedgerInstructions('Ethereum'),
       {
         icon: 'fa-cogs',
         text: 'Ensure that Browser Support is enabled in Settings'
@@ -41,30 +45,39 @@ const instructions = {
         text: 'You may need to update the firmware if Browser Support is not available'
       },
     ],
-    BTC: ledgerInstructions('Bitcoin'),
   },
   trezor: {
     ETH: [
-      ...trezorInstructions(),
+      ...defaultTrezorInstructions('Ethereum').slice(0, 2),
       {
         icon: 'fa-window-restore',
         text: (<span>When asked if you want to export the public key, select <b>Export</b></span>)
       },
     ],
-    BTC: [
-      ...trezorInstructions(),
-      {
-        icon: 'fa-th-list',
-        text: 'When shown a list of accounts, select the one you want to use',
-      }
-    ]
   }
 }
 
-const ConnectionInstructions = ({ type, symbol }) => (
+const defaultInstructionCreators = {
+  ledger: defaultLedgerInstructions,
+  trezor: defaultTrezorInstructions,
+}
+
+function getInstructions(type, asset) {
+  const inst = (assetInstructionOverrides[type] || {})[asset.symbol]
+  if (inst) {
+    return inst
+  }
+  const instCreator = defaultInstructionCreators[type]
+  if (instCreator) {
+    return instCreator(asset.name)
+  }
+  return []
+}
+
+const ConnectionInstructions = ({ type, asset }) => (
   <table className='mx-0 mx-md-5'>
     <tbody>
-      {((instructions[type] || {})[symbol] || []).map(({ icon, text }) => (
+      {getInstructions(type, asset).map(({ icon, text }) => (
         <tr key={text}>
           <td className='text-center'>
             <i className={classNames('fa fa-2x', icon)} />
@@ -79,8 +92,11 @@ const ConnectionInstructions = ({ type, symbol }) => (
 )
 
 ConnectionInstructions.propTypes = {
-  type: PropTypes.oneOf(Object.keys(instructions)).isRequired,
-  symbol: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(Object.keys(defaultInstructionCreators)).isRequired,
+  asset: PropTypes.shape({
+    symbol: PropTypes.string,
+    name: PropTypes.string,
+  })
 }
 
 export default ConnectionInstructions
