d6fc1d657c59e7e4cc6d4a50f8507901c0eb0bec
Dylan Seago <dylan.seago@gmail.com>
2018-10-22 16:09:36 -0400

    Fix bchaddrjs type defs

--- src/@types/bchaddrjs/index.d.ts
+++ src/@types/bchaddrjs/index.d.ts
@@ -8,26 +8,24 @@ declare module 'bchaddrjs' {
   type AddrNetwork = 'mainnet' | 'testnet'
   type AddrType = 'p2pkh' | 'p2sh'
 
-  interface BchAddr {
-    Format: { Legacy: 'legacy', Bitpay: 'bitpay', Cashaddr: 'cashaddr' }
-    Network: { Mainnet: 'mainnet', Testnet: 'testnet' }
-    Type: { P2PKH: 'p2pkh', P2SH: 'p2sh' }
-    detectAddressFormat(address: string): AddrFormat
-    detectAddressNetwork(address: string): AddrNetwork
-    detectAddressType(address: string): AddrType
-    toLegacyAddress(address: string): string
-    toBitpayAddress(address: string): string
-    toCashAddress(address: string): string
-    isLegacyAddress(address: string): boolean
-    isBitpayAddress(address: string): boolean
-    isCashAddress(address: string): boolean
-    isMainnetAddress(address: string): boolean
-    isTestnetAddress(address: string): boolean
-    isP2PKHAddress(address: string): boolean
-    isP2SHAddress(address: string): boolean
-    InvalidAddressError: Error
+  export = {
+    Format: { Legacy: 'legacy', Bitpay: 'bitpay', Cashaddr: 'cashaddr' },
+    Network: { Mainnet: 'mainnet', Testnet: 'testnet' },
+    Type: { P2PKH: 'p2pkh', P2SH: 'p2sh' },
+    detectAddressFormat: (address: string) => AddrFormat,
+    detectAddressNetwork: (address: string) => AddrNetwork,
+    detectAddressType: (address: string) => AddrType,
+    toLegacyAddress: (address: string) => string,
+    toBitpayAddress: (address: string) => string,
+    toCashAddress: (address: string) => string,
+    isLegacyAddress: (address: string) => boolean,
+    isBitpayAddress: (address: string) => boolean,
+    isCashAddress: (address: string) => boolean,
+    isMainnetAddress: (address: string) => boolean,
+    isTestnetAddress: (address: string) => boolean,
+    isP2PKHAddress: (address: string) => boolean,
+    isP2SHAddress: (address: string) => boolean,
+    InvalidAddressError: Error,
   }
-
-  export { BchAddr as default }
 
 }
