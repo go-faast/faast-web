cd3c4cfd32694cefd19927f448cfb69b863e41d5
Dylan Seago <dylan.seago@gmail.com>
2018-11-03 16:12:21 -0400

    Update ledger transport type

--- src/@types/@ledgerhq/hw-transport-u2f/index.d.ts
+++ src/@types/@ledgerhq/hw-transport-u2f/index.d.ts
@@ -4,5 +4,6 @@ declare module '@ledgerhq/hw-transport-u2f' {
   export default class TransportU2F {
     static create(): Promise<TransportU2F>
     setExchangeTimeout(timeout: number): void
+    setDebugMode(on: boolean): void
   }
 }
