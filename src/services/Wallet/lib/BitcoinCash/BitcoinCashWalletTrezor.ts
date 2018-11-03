b0d9bd5ec4bb820424ed51882c548ddc60fa68ca
Dylan Seago <dylan.seago@gmail.com>
2018-11-03 14:21:15 -0400

    Use instance _network

--- src/services/Wallet/lib/BitcoinCash/BitcoinCashWalletTrezor.ts
+++ src/services/Wallet/lib/BitcoinCash/BitcoinCashWalletTrezor.ts
@@ -7,7 +7,6 @@ import BitcoinCashWallet from './BitcoinCashWallet'
 import { BitcoreTransaction } from '../types'
 
 const typeLabel = config.walletTypes.trezor.name
-const network = networks.BCH
 
 export default class BitcoinCashWalletTrezor extends BitcoinCashWallet {
 
@@ -18,11 +17,11 @@ export default class BitcoinCashWalletTrezor extends BitcoinCashWallet {
   getTypeLabel() { return typeLabel }
 
   static fromPath(derivationPath?: string | null) {
-    return Trezor.getHdAccount(network, derivationPath)
+    return Trezor.getHdAccount(networks.BCH, derivationPath)
       .then(({ xpub, path }) => new BitcoinCashWalletTrezor(xpub, path))
   }
 
   _signTx({ txData }: BitcoreTransaction): Promise<Partial<BitcoreTransaction>> {
-    return Trezor.signPaymentTx(network, this.derivationPath, txData)
+    return Trezor.signPaymentTx(this._network, this.derivationPath, txData)
   }
 }
