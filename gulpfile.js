74bdd6ea99b091fcd4c503c5e19e4e4ab054f8df
Dylan Seago <dylan.seago@gmail.com>
2018-11-15 18:23:06 -0500

    Use gulp for entire build pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,34 @@
 const gulp = require('gulp')
 const path = require('path')
 const clean = require('gulp-clean')
+const run = require('gulp-run-command').default
+const sequence = require('run-sequence')
 
 const { dirs } = require('./etc/common.js')
- 
-gulp.task('dist-clean', () =>
+
+const series = (...args) => (cb) => sequence(...args, cb)
+
+gulp.task('lint:js', run('eslint src/'))
+gulp.task('lint:ts', run('tslint -p .'))
+gulp.task('lint', ['lint:js', 'lint:ts'])
+
+gulp.task('compile:app', run('webpack --config etc/webpack.config.app.js --progress'))
+gulp.task('compile:site', run('react-static build'))
+
+gulp.task('clean', () =>
   gulp.src(dirs.dist, { read: false })
     .pipe(clean()))
 
-gulp.task('dist', ['dist-clean'], () =>
-  gulp.src([
-    path.join(dirs.buildSite, '**/*'),
-    path.join(dirs.buildApp, '**/*'),
-  ]).pipe(gulp.dest(dirs.dist)))
+gulp.task('dist:app', () =>
+  gulp.src(path.join(dirs.buildSite, '**/*'))
+    .pipe(gulp.dest(dirs.dist)))
+
+gulp.task('dist:site', () =>
+  gulp.src(path.join(dirs.buildSite, '**/*'))
+    .pipe(gulp.dest(dirs.dist)))
+
+gulp.task('build:app', series('compile:app', 'dist:app'))
+gulp.task('build:site', series('compile:site', 'dist:site'))
+gulp.task('build', series('lint', 'clean', ['build:app', 'build:site']))
+
+gulp.task('default', ['build'])
