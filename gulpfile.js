f3e6dc8258f8546c8d9739aee6d7c0208ca84efe
Dylan Seago <dylan.seago@gmail.com>
2018-11-15 19:19:46 -0500

    Upgrade gulpfile to v4 compat

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,17 @@ const gulp = require('gulp')
 const path = require('path')
 const clean = require('gulp-clean')
 const run = require('gulp-run-command').default
-const sequence = require('run-sequence')
 
 const { dirs } = require('./etc/common.js')
 
-const series = (...args) => (cb) => sequence(...args, cb)
+const ignoredFiles = [
+  '!' + path.join(dirs.buildSite, '**/_*'), // exclude folders starting with '_'
+  '!' + path.join(dirs.buildSite, '**/_*/**/*'), // exclude files/subfolders in folders starting with '_'
+]
 
 gulp.task('lint:js', run('eslint src/'))
 gulp.task('lint:ts', run('tslint -p .'))
-gulp.task('lint', ['lint:js', 'lint:ts'])
+gulp.task('lint', gulp.parallel(['lint:js', 'lint:ts']))
 
 gulp.task('compile:app', run('webpack --config etc/webpack.config.app.js --progress'))
 gulp.task('compile:site', run('react-static build'))
@@ -19,16 +21,21 @@ gulp.task('clean', () =>
   gulp.src(dirs.dist, { read: false })
     .pipe(clean()))
 
-gulp.task('dist:app', () =>
-  gulp.src(path.join(dirs.buildSite, '**/*'))
-    .pipe(gulp.dest(dirs.dist)))
+const mergeDist = (path) =>
+  gulp.src([
+    path,
+    ...ignoredFiles,
+  ]).pipe(gulp.dest(dirs.dist, { overwrite: false }))
 
-gulp.task('dist:site', () =>
-  gulp.src(path.join(dirs.buildSite, '**/*'))
-    .pipe(gulp.dest(dirs.dist)))
+gulp.task('dist:app', () => mergeDist(path.join(dirs.buildApp, '**/*')))
 
-gulp.task('build:app', series('compile:app', 'dist:app'))
-gulp.task('build:site', series('compile:site', 'dist:site'))
-gulp.task('build', series('lint', 'clean', ['build:app', 'build:site']))
+gulp.task('dist:site', () => mergeDist(path.join(dirs.buildSite, '**/*')))
 
-gulp.task('default', ['build'])
+gulp.task('build:app', gulp.series('compile:app', 'dist:app'))
+gulp.task('build:site', gulp.series('compile:site', 'dist:site'))
+
+const build = gulp.series('lint', 'clean', gulp.parallel(['build:app', 'build:site']))
+
+gulp.task('build', build)
+
+gulp.task('default', build)
