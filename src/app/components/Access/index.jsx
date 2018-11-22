58626da1b405a9e7ad7e39896c521f2c670f1490
Dylan Seago <dylan.seago@gmail.com>
2018-11-22 13:46:58 -0500

    Show mobile options on desktop

--- src/app/components/Access/index.jsx
+++ src/app/components/Access/index.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Row, Col, Button } from 'reactstrap'
 import { name as appName, version as appVersion } from 'Pkg'
 import CoinIcon from 'Components/CoinIcon'
-import classNames from 'class-names'
 import Web3Wallet from './Web3Wallet'
 import CreateWallet from './CreateWallet'
 import ImportKeystore from './ImportKeystore'
@@ -34,7 +33,7 @@ const Access = () => (
           <TileCol><Web3Wallet type='mist' /></TileCol>
         </TileRow>
       </Col>
-      <Col xs='12' className={classNames('d-lg-none d-block', MOBILE_ORDER)}>
+      <Col xs='12' className={MOBILE_ORDER}>
         <h4 className='text-muted'><CoinIcon symbol='ETH' size='sm' inline /> Ethereum Mobile Wallets</h4>
         <TileRow>
           <TileCol><Web3Wallet type='trust' /></TileCol>
