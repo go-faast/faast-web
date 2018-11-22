258860bc5985f28b92428f994f0ae5cdb6b4e17f
Dylan Seago <dylan.seago@gmail.com>
2018-11-22 14:40:29 -0500

    Add app store links for coinbase and status wallets

--- src/app/components/WalletInfoModal/index.jsx
+++ src/app/components/WalletInfoModal/index.jsx
@@ -6,29 +6,44 @@ import { pick } from 'lodash'
 
 import Icon from 'Components/Icon'
 import config from 'Config'
+import AppStoreLinks from 'Components/AppStoreLinks'
+
+const { walletTypes } = config
+const { metamask, status, mist, coinbase } = walletTypes
 
 const walletInformation = {
   metamask: (
     <Fragment>
-      MetaMask is a desktop browser extension that provides an Ethereum wallet. You can get it <a href={config.walletTypes.metamask.website}>here.</a>
-    </Fragment>
-  ),
-  status: (
-    <Fragment>
-      Status is a mobile Ethereum OS for iOS and Android. You can learn more about it <a href={config.walletTypes.status.website}>here.</a> 
-      <p className='mt-2'>If you already have Status, open <a href='https://faa.st'>Faa.st</a> using its built in web browser.</p>
+      <p>{metamask.name} is a desktop browser extension that provides an Ethereum wallet. You can get it <a href={metamask.website}>here.</a></p>
+      <p>If you already have {metamask.name}, ensure the extension is enabled on this browser.</p>
     </Fragment>
   ),
   mist: (
     <Fragment>
-      Mist Browser is a desktop web browser with a built-in Ethereum wallet. You can get it <a href={config.walletTypes.mist.website}>here.</a>
+      <p>{mist.name} is a desktop web browser with a built-in Ethereum wallet. You can get it <a href={mist.website}>here.</a></p>
+      <p>If you already have {mist.name}, open <a href='https://faa.st/app'>faa.st</a> using its built in web browser.</p>
+    </Fragment>
+  ),
+  status: (
+    <Fragment>
+      <p>{status.name} is a mobile Ethereum OS for iOS and Android. You can learn more about it <a href={status.website}>here.</a></p>
+      <div className='mb-3'>
+        <AppStoreLinks
+          google='https://play.google.com/store/apps/details?id=im.status.ethereum'
+          apple='https://testflight.apple.com/join/J8EuJmey'/>
+      </div>
+      <p>If you already have {status.name}, open <a href='https://faa.st/app'>faa.st</a> using its built in web browser.</p>
     </Fragment>
   ),
   coinbase: (
     <Fragment>
-      Coinbase Wallet is a mobile Ethereum wallet for iOS and Android. 
-      You can get it <a href={config.walletTypes.coinbase.website}>here.</a>
-      <p className='mt-2'>If you already have Coinbase Wallet, open <a href='https://faa.st'>Faa.st</a> using its built in web browser.</p>
+      <p>{coinbase.name} is a mobile Ethereum wallet for iOS and Android. You can get it <a href={coinbase.website}>here</a> or use one of the links below.</p>
+      <div className='mb-3'>
+        <AppStoreLinks
+          google='https://play.google.com/store/apps/details?id=org.toshi'
+          apple='https://itunes.apple.com/app/coinbase-wallet/id1278383455?ls=1&amp;mt=8'/>
+      </div>
+      <p>If you already have {coinbase.name}, open <a href='https://faa.st/app'>faa.st</a> using its built in web browser.</p>
     </Fragment>
   )
 }
@@ -46,7 +61,7 @@ export default compose(
     size='md' toggle={toggle} className='mt-6 mx-md-auto' contentClassName='p-0'
     {...pick(props, Object.keys(Modal.propTypes))}>
     <ModalHeader tag='h4' toggle={toggle} className='text-primary'>
-      <Icon key='icon' src={config.walletTypes[walletType].icon} height='20px' className='m-2' /> About {config.walletTypes[walletType].name}
+      <Icon key='icon' src={walletTypes[walletType].icon} height='20px' className='m-2' /> {walletTypes[walletType].name}
     </ModalHeader>
     <ModalBody className='p-2 p-sm-3'>
       {walletInformation[walletType]}
