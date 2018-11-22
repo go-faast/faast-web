60b7bcdbb750d650511828c337d9e091d3d80d15
Dylan Seago <dylan.seago@gmail.com>
2018-11-22 15:15:30 -0500

    Fix linting script and move back into package.json

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,12 @@ const ignoredFiles = [
   '!' + path.join(dirs.buildSite, '**/_*/**/*'), // exclude files/subfolders in folders starting with '_'
 ]
 
-gulp.task('lint:js', run('eslint src/'))
-gulp.task('lint:ts', run('tslint -p .'))
+gulp.task('lint:js', run('npm run lint:js'))
+gulp.task('lint:ts', run('npm run lint:ts'))
 gulp.task('lint', gulp.parallel(['lint:js', 'lint:ts']))
 
-gulp.task('compile:app', run('webpack --config etc/webpack.config.app.js --progress'))
-gulp.task('compile:site', run('react-static build'))
+gulp.task('compile:app', run('npm run compile:app'))
+gulp.task('compile:site', run('npm run compile:site'))
 
 gulp.task('clean', () =>
   gulp.src(dirs.dist, { read: false })
