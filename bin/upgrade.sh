929ac25bbed76f6fb3e1fa2a4e54d16e81cf78c3
Matus Dubrava <dankonino@gmail.com>
2018-11-22 16:42:49 +0100

    make deployments great again

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -3,6 +3,20 @@
 RELEASE=$(git tag -l | grep $(git describe --tags))
 if [ -n "$RELEASE"]; then
   npm run release
-fi
 
-#
+  echo npm run release done, now triggering build on faast-swap
+
+  curl -sL -u $BUILDUSER=:$BUILDPASSWORD -X POST \
+    -H 'Content-Type: application/json' \
+   https://api.bitbucket.org/2.0/repositories/bitaccess/faast-swap/pipelines/ \
+    -d '
+    {
+      "target": {
+        "ref_type": "branch",
+        "type": "pipeline_ref_target",
+        "ref_name": "upgrade"
+      }
+    }'
+else
+  echo no release tag, just passing by
+fi
