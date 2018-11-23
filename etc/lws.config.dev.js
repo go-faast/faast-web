ab1cfc427bb71291046e327b904768e3b76e9545
Dylan Seago <dylan.seago@gmail.com>
2018-11-23 13:20:45 -0500

    Specify proxy port from cli

--- etc/lws.config.dev.js
+++ etc/lws.config.dev.js
@@ -3,10 +3,12 @@ const { useHttps } = require('./common.js')
 const proto = useHttps ? 'https' : 'http'
 const appDomain = `${proto}://localhost:8080`
 const siteDomain = `${proto}://localhost:3000`
+const port = Number.parseInt(process.env.PROXY_PORT || 8000)
 
 module.exports = {
   https: useHttps,
   hostname: 'localhost',
+  port,
   rewrite: [{
     from: '/app',
     to: `${appDomain}/app`,
