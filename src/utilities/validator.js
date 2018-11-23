eecc579b2e1d37444ae52d233c4ea934784be14d
Dylan Seago <dylan.seago@gmail.com>
2018-11-23 12:35:28 -0500

    Add validators for gt,gte,lt,lte

--- src/utilities/validator.js
+++ src/utilities/validator.js
@@ -1,4 +1,5 @@
 import { getDefaultFormat } from 'Utilities/addressFormat'
+import { toBigNumber } from 'Utilities/convert'
 
 const isProvided = (x) => typeof x !== 'undefined' && x !== null && x !== ''
 const isChecked = (x) => typeof x !== 'undefined' && x !== null && x !== false
@@ -43,13 +44,32 @@ export function number() {
   }
 }
 
-export function greaterThan(x, errorMessage = `Must be greater than ${x}`) {
+function comparative(comparator, x, errorMessage) {
+  x = toBigNumber(x)
   return (value) => {
-    if (isNumber(value) && value <= x) {
+    if (isNumber(value) && !(comparator(toBigNumber(value), x))) {
       return errorMessage
     }
   }
 }
+
+export function greaterThan(x, errorMessage = `Must be greater than ${x}`) {
+  return comparative((a, b) => a.gt(b), x, errorMessage)
+}
+
+export function greaterThanOrEqualTo(x, errorMessage = `Must be at least ${x}`) {
+  return comparative((a, b) => a.gte(b), x, errorMessage)
+}
+
+export function lessThan(x, errorMessage = `Must be less than ${x}`) {
+  return comparative((a, b) => a.lt(b), x, errorMessage)
+}
+
+export function lessThanOrEqualTo(x, errorMessage = `Must be at most ${x}`) {
+  return comparative((a, b) => a.lte(b), x, errorMessage)
+}
+
+export { greaterThan as gt, greaterThanOrEqualTo as gte, lessThan as lt, lessThanOrEqualTo as lte }
 
 function getAddressValidator(asset) {
   const { symbol, ERC20 } = asset
