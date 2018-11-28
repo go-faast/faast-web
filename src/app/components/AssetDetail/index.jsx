55f183df182a1c418d7db26a2890ce876a273d0d
Dylan Seago <dylan.seago@gmail.com>
2018-11-28 16:02:37 -0500

    Remove unused import

--- src/app/components/AssetDetail/index.jsx
+++ src/app/components/AssetDetail/index.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
