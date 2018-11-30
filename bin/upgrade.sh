990915ee377cfd997f4cca6cf8c835ba88696ac9
Dylan Seago <dylan.seago@gmail.com>
2018-11-30 11:49:39 -0500

    Exit upgrade.sh early if not release

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 RELEASE=$(git tag -l | grep $(git describe --tags))
+
+if [ -z "$RELEASE" ]; then
+  echo no release tag, just passing by
+fi
+
 echo $RELEASE
 
 echo doing the necessary git Config
@@ -10,23 +15,19 @@ for branch in $(git branch -r|grep -v HEAD) ; do
     git checkout ${branch#origin/}
 done
 
-if [ -n "$RELEASE" ]; then
-  echo npm run release
-  npm run release $RELEASE
+echo npm run release
+npm run release $RELEASE
 
-  echo npm run release done, now triggering build on faast-swap
+echo npm run release done, now triggering build on faast-swap
 
-  curl -sL -u $BUILDUSER=:$BUILDPASSWORD -X POST \
-    -H 'Content-Type: application/json' \
-   https://api.bitbucket.org/2.0/repositories/bitaccess/faast-swap/pipelines/ \
-    -d '
-    {
-      "target": {
-        "ref_type": "branch",
-        "type": "pipeline_ref_target",
-        "ref_name": "upgrade"
-      }
-    }'
-else
-  echo no release tag, just passing by
-fi
+curl -sL -u $BUILDUSER=:$BUILDPASSWORD -X POST \
+  -H 'Content-Type: application/json' \
+  https://api.bitbucket.org/2.0/repositories/bitaccess/faast-swap/pipelines/ \
+  -d '
+  {
+    "target": {
+      "ref_type": "branch",
+      "type": "pipeline_ref_target",
+      "ref_name": "upgrade"
+    }
+  }'
