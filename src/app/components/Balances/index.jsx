6c699b680579321b63b6e0c20c1a488f299117ef
dwolfgram <daniel.j.wolfgram@gmail.com>
2018-12-04 21:42:36 -0800

    header box share / remove

--- src/app/components/Balances/index.jsx
+++ src/app/components/Balances/index.jsx
@@ -1,19 +1,23 @@
 import React, { Fragment } from 'react'
 import { createStructuredSelector } from 'reselect'
 import PropTypes from 'prop-types'
-import { Card, CardHeader, CardBody
+import { Card, CardHeader, CardBody, Col, Row, Button, Dropdown, DropdownMenu, DropdownToggle,
+  DropdownItem
 } from 'reactstrap'
 import { connect } from 'react-redux'
+
+import withToggle from 'Hoc/withToggle'
 
 import { getWalletWithHoldings } from 'Selectors'
 import Address from 'Components/Address'
 import LoadingFullscreen from 'Components/LoadingFullscreen'
 import PieChart from 'Components/PieChart'
 import AssetTable from 'Components/AssetTable'
+import ShareButton from 'Components/ShareButton'
 
-const Balances = ({ wallet }) => {
+const Balances = ({ wallet, handleRemove, isDropdownOpen, toggleDropdownOpen }) => {
   const {
-    address, assetHoldings, holdingsLoaded, holdingsError
+    address, assetHoldings, holdingsLoaded, holdingsError, label
   } = wallet
 
   const assetRows = assetHoldings.filter(({ shown }) => shown)
@@ -25,7 +29,33 @@ const Balances = ({ wallet }) => {
       )}
       <Card>
         <CardHeader>
-          <h5>Holdings</h5>
+          <Col className='px-0' xs='12'>
+            <Row className='gutter-3 align-items-center'>
+              <Col className='px-2'>
+                <h5>{address ? label : 'Portfolio'} Holdings</h5>
+              </Col>
+              {address ?
+                (<Fragment>
+                  <Col style={{ top: '2px' }} className='p-0 position-relative' xs='auto'>
+                    <ShareButton wallet={wallet}/>
+                  </Col>
+                  <Col className='p-0' xs='auto'>
+                    <Dropdown group isOpen={isDropdownOpen} size="sm" toggle={toggleDropdownOpen}>
+                      <DropdownToggle 
+                        tag='div'
+                        className='py-0 px-2 flat d-inline-block position-relative cursor-pointer' 
+                      >
+                        <i className="fa fa-ellipsis-v" aria-hidden="true"></i>
+                      </DropdownToggle>
+                      <DropdownMenu right>
+                        <DropdownItem onClick={handleRemove}>Remove Wallet</DropdownItem>
+                      </DropdownMenu>
+                    </Dropdown>
+                  </Col>
+                </Fragment>)
+                : null}
+            </Row>
+          </Col>
         </CardHeader>
         <AssetTable assetRows={assetRows}/>
       </Card>
@@ -48,7 +78,8 @@ const Balances = ({ wallet }) => {
 }
 
 Balances.propTypes = {
-  wallet: PropTypes.object.isRequired
+  wallet: PropTypes.object.isRequired,
+  removeButton: PropTypes.node
 }
 
 const ConnectedBalances = connect(createStructuredSelector({
@@ -61,5 +92,6 @@ ConnectedBalances.propTypes = {
 
 Balances.Connected = ConnectedBalances
 
+
 export { Balances, ConnectedBalances }
-export default Balances
+export default withToggle('dropdownOpen')(Balances)
