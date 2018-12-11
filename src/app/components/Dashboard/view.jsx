a8a16c5591d4adc25cc28c7f557d907b78e58b35
Dylan Seago <dylan.seago@gmail.com>
2018-12-11 16:23:58 -0500

    Remove extra margin from sidebar

--- src/app/components/Dashboard/view.jsx
+++ src/app/components/Dashboard/view.jsx
@@ -7,7 +7,7 @@ import Layout from 'Components/Layout'
 import BlockstackWelcome from 'Components/BlockstackWelcome'
 import Balances from 'Components/Balances'
 import Sidebar from 'Components/Sidebar'
-import WalleSelector from 'Components/WalletSelector'
+import WalletSelector from 'Components/WalletSelector'
 import TradeTable from 'Components/TradeTable'
 import { tableHeadings } from 'Components/TradeHistory'
 
@@ -24,9 +24,9 @@ const DashboardView = (props) => {
       }
       <Row className='gutter-3'>
         {!isDefaultPortfolioEmpty && (
-          <Col className='mt-2' xs='12' md='5' lg='4' xl='3'>
+          <Col xs='12' md='5' lg='4' xl='3'>
             <Sidebar className='d-none d-md-block'/>
-            <WalleSelector className='d-flex d-md-none'/>
+            <WalletSelector className='d-flex d-md-none'/>
           </Col>
         )}
         <Col xs='12' md='7' lg='8' xl='9'>
