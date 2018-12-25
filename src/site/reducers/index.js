8e8f1e4014853958d2b55fd61093fa9e5cc53bc4
dwolfgram <daniel.j.wolfgram@gmail.com>
2018-12-25 15:17:23 -0500

    add formReducer to static

--- src/site/reducers/index.js
+++ src/site/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux'
 
 import asset from 'Common/reducers/asset'
 import app from 'Common/reducers/app'
+import { reducer as formReducer } from 'redux-form'
 
 const reducer = combineReducers({
   asset,
-  app
+  app,
+  form: formReducer,
 })
 
 export default reducer
