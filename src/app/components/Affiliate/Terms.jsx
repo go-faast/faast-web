619a86a34da59a3f760ac7a2672de6c541395c86
dwolfgram <daniel.j.wolfgram@gmail.com>
2019-01-02 18:24:30 -0700

    unescaped terms

--- src/app/components/Affiliate/Terms.jsx
+++ src/app/components/Affiliate/Terms.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 import { compose, setDisplayName } from 'recompose'
 import { Card, CardHeader, CardBody } from 'reactstrap'
