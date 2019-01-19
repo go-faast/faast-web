c2cf64675cbcc809e7165f37e17e48c9ff7cde8c
dwolfgram <daniel.j.wolfgram@gmail.com>
2019-01-18 18:23:29 -0700

    remove link

--- src/site/components/IconCarousel/index.jsx
+++ src/site/components/IconCarousel/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {
   compose, setDisplayName, setPropTypes, withProps, withState, withHandlers,
