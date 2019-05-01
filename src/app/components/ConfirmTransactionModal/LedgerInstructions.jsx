16c01bb91bf050930305678c7068d2e93d6d6f59
dwolfgram <daniel.j.wolfgram@gmail.com>
2019-05-01 16:11:44 -0400

    app

--- src/app/components/ConfirmTransactionModal/LedgerInstructions.jsx
+++ src/app/components/ConfirmTransactionModal/LedgerInstructions.jsx
@@ -1,8 +1,11 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 import PropTypes from 'prop-types'
 import { compose, setDisplayName, setPropTypes } from 'recompose'
 import LedgerScreen from './LedgerScreen'
 import Timer from '../Timer'
+
+import T from 'Components/i18n/T'
 
 export default compose(
   setDisplayName('LedgerInstructions'),
@@ -15,12 +18,11 @@ export default compose(
   })
 )(({ appName, screens }) => (
   <div>
-    <p>Please confirm the transaction on your device. You should see the following information on screen.</p>
-    <p><small>
-      {'Tip: If you don\'t see the transaction on your device, ensure you\'ve unlocked '
-      + `it using your PIN and have opened the ${appName} app.`}
-    </small></p>
-    <small><Timer className='text-warning' seconds={120} label={'Please confirm within:'}/></small>
+    <T tag='p' i18nKey='app.ledgerInstructions.pleaseConfirm'>Please confirm the transaction on your device. You should see the following information on screen.</T>
+    <T tag='p' i18nKey='app.ledgerInstructions.tip'><small>
+      Tip: If you don't see the transaction on your device, ensure you've unlocked it using your PIN and have opened the {appName} app.
+    </small></T>
+    <small><Timer className='text-warning' seconds={120} label={<T tag='span' i18nKey='app.ledgerInstructions.confirmWithin'>Please confirm within:</T>}/></small>
     {screens.map(({ top, bottom }, i) => (
       <LedgerScreen key={i} top={top} bottom={bottom}/>
     ))}
--- src/app/components/ConfirmTransactionModal/TrezorEthInstructions.jsx
+++ src/app/components/ConfirmTransactionModal/TrezorEthInstructions.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { compose, setDisplayName, setPropTypes } from 'recompose'
 import Units from 'Components/Units'
+import T from 'Components/i18n/T'
 
 import TrezorInstructions from './TrezorInstructions';
 
@@ -13,19 +14,19 @@ export default compose(
 )(({ tx: { outputs: [{ amount, address }], asset: { symbol, ERC20 }, feeAmount, feeSymbol, feeAsset } }) => (
   <TrezorInstructions appName={feeAsset.name} screens={[(
     <span key='1'>
-      Send <br/>
+      <T tag='span' i18nKey='app.trezorETHInstructions.send'>Send</T> <br/>
       <Units value={amount} symbol={symbol} precision={null}/><br/>
-      to {address.slice(0,12)}<br/>
+      <T tag='span' i18nKey='app.trezorETHInstructions.to'>to</T> {address.slice(0,12)}<br/>
       {address.slice(12,27)}<br/>
       {address.slice(27)}?
     </span>
   ), (
       <span key='2'>
-      Really send <br/>
+        <T tag='span' i18nKey='app.trezorETHInstructions.reallySend'>Really send</T> <br/>
         {ERC20 ? 'token' : (<Units value={amount} symbol={symbol} precision={null}/>)} <br/>
-      paying up to <br/>
+        <T tag='span' i18nKey='app.trezorETHInstructions.paying'>paying up to</T> <br/>
         <Units value={feeAmount} symbol={feeSymbol} precision={null}/><br/>
-      for gas?
+        <T tag='span' i18nKey='app.trezorETHInstructions.forGas'>for gas?</T>
       </span>
     )]}/>
 ))
