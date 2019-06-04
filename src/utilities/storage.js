bf123fa73e66e810694ed9549eca1624e2479d81
dwolfgram <daniel.j.wolfgram@gmail.com>
2019-06-04 17:58:42 -0400

    check for window ls

--- src/utilities/storage.js
+++ src/utilities/storage.js
@@ -10,18 +10,22 @@ export const storageAvailable = (type) => {
     storage.removeItem(x)
     return true
   } catch (e) {
-    return e instanceof window.DOMException && (
-      // everything except Firefox
-      e.code === 22 ||
-      // Firefox
-      e.code === 1014 ||
-      // test name field too, because code might not be present
-      // everything except Firefox
-      e.name === 'QuotaExceededError' ||
-      // Firefox
-      e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-      // acknowledge QuotaExceededError only if there's something already stored
-      storage.length !== 0
+    if (window) {
+      return e instanceof window.DOMException && (
+        // everything except Firefox
+        e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === 'QuotaExceededError' ||
+        // Firefox
+        e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+        // acknowledge QuotaExceededError only if there's something already stored
+        storage.length !== 0
+    } else {
+      return 
+    }
   }
 }
 
