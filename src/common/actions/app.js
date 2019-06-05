667c0342a2312eba314661d9bd3353cb3a914633
dwolfgram <daniel.j.wolfgram@gmail.com>
2019-06-04 20:53:33 -0400

    local storage >:(

--- src/common/actions/app.js
+++ src/common/actions/app.js
@@ -1,5 +1,6 @@
 import { newScopedCreateAction } from 'Utilities/action'
 import log from 'Utilities/log'
+import { localStorageSet, localStorageGet } from 'Utilities/storage'
 import Faast from 'Services/Faast'
 
 const createAction = newScopedCreateAction(__filename)
@@ -9,13 +10,11 @@ export const restrictionsError = createAction('RESTRICTIONS_ERROR')
 export const updateLanguage = createAction('UPDATE_LANGUAGE', (language) => ({ language }))
 
 export const staticAppLoad = () => (dispatch) => {
-  if (localStorage) {
-    let lang = localStorage.getItem('i18nextLng') || 'en'
-    if (lang.indexOf('-') >= 0) {
-      lang = lang.substring(0, lang.indexOf('-')).toLowerCase()
-    }
-    dispatch(selectLanguage(lang))
+  let lang = localStorageGet('i18nextLng') || 'en'
+  if (lang.indexOf('-') >= 0) {
+    lang = lang.substring(0, lang.indexOf('-')).toLowerCase()
   }
+  dispatch(selectLanguage(lang))
 }
 
 export const fetchGeoRestrictions = () => (dispatch) => Promise.resolve()
@@ -29,7 +28,5 @@ export const fetchGeoRestrictions = () => (dispatch) => Promise.resolve()
 
 export const selectLanguage = (lang) => (dispatch) => {
   dispatch(updateLanguage(lang))
-  if (localStorage) {
-    localStorage.setItem('i18nextLng', lang)
-  }
+  localStorageSet('i18nextLng', lang)
 }
--- src/site/config.js
+++ src/site/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'Faa.st',
   author: 'Bitaccess',
-  title: 'Trade Crypto - Faa.st',
-  description: 'Faast is the safest and fastest way to build a crypto currency portfolio. Connect your hardware wallet, metamask, or other ethereum wallet and let us do the work for you. Easily trade over 100 different tokens for near-zero fees.',
+  title: 'Faa.st: Safely Trade Cryptocurrencies Directly from Your Wallet',
+  description: 'Faa.st is the safest and fastest way to build a cryptocurrency portfolio that includes coins like Bitcoin, Ethereum, Litecoin, and Monero. Connect your wallet and easily trade over 100 different tokens for near-zero fees.',
   year: new Date().getFullYear(),
 }
