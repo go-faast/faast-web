8c168371d632221e69f626f041e16e0dd12877f2
dwolfgram <daniel.j.wolfgram@gmail.com>
2019-07-23 00:46:00 -0400

    langing page with  email form

--- src/site/pages/NewsletterLanding/index.jsx
+++ src/site/pages/NewsletterLanding/index.jsx
@@ -2,9 +2,11 @@
 import React, { Fragment } from 'react'
 import { compose, setDisplayName } from 'recompose'
 import { withRouteData } from 'react-static'
+import Icon from 'Components/Icon'
 import withTracker from 'Site/components/withTracker'
 import Header from 'Site/components/Header'
 import Footer from 'Site/components/Footer'
+import EmailForm from 'Site/components/InlineEmailForm'
 import { Row, Col } from 'reactstrap'
 
 import classNames from 'class-names'
@@ -13,7 +15,9 @@ import BitcoinLogo from 'Img/newsletter-btc.svg'
 import EthereumLogo from 'Img/newsletter-eth.svg'
 import LitecoinLogo from 'Img/newsletter-ltc.svg'
 
-import { hero, eth, ltc, btc, dashedLine, line } from './style.scss'
+import SwapIcon from 'Img/swap-icon.svg'
+
+import { hero, eth, ltc, btc, dashedLine, line, circle, swapIcon, icon, coinContainer, dashedLineBlue } from './style.scss'
 
 export default compose(
   setDisplayName('NewsletterLanding'),
@@ -25,7 +29,7 @@ export default compose(
       <div className='position-relative' style={{ zIndex: 9999 }}>
         <Header translations={translations} headerColor={'transparent'} />
       </div>
-      <div className='position-absolute' style={{ top: 68, right: '45%' }}>
+      <div className={coinContainer}>
         <div className={classNames(eth, line)}>
           <img src={EthereumLogo} style={{ width: 91, height: 93 }} />
         </div>
@@ -36,17 +40,53 @@ export default compose(
           <img src={BitcoinLogo} style={{ width: 71, height: 72 }} />
         </div>
         <div className={dashedLine}></div>
+        <div className={dashedLineBlue}></div>
       </div>
       <Row className='p-0 m-0 position-relative' style={{ zIndex: 999 }}>
-        <Col className='pl-5' sm='6'>
+        <Col className='pl-md-5 pl-0 mt-4' sm='7'>
           <div className='pl-3'>
-            <h1 style={{ fontWeight: 600 }} className='mt-5 pt-5'>Keep up with the Faa.st Newsletter</h1>
+            <h1 style={{ fontWeight: 600, fontSize: 37 }} className='mt-5 pt-5'>Keep up with the Faa.st Newsletter</h1>
             <h5 className='mt-4' style={{ color: '#B6AFDC', fontSize: 16 }}>
             Stay up-to-date on all that is happening with Faa.st including adding new assets, new features, cryptocurrency news, and more!
             </h5>
+            <div style={{ maxWidth: 505, overflow: 'hidden' }} className='mt-4 pt-1'>
+              <EmailForm />
+            </div>
+            <div className='position-relative' style={{ top: -80 }}>
+              <div className={circle}>
+                <Row className='align-content-center'>
+                  <Col xs='12'>
+                    <i className={classNames(icon, 'fa fa-lock')}></i>
+                  </Col>
+                  <Col xs='12'>
+                    <small>secure</small>
+                  </Col>
+                </Row>
+              </div>
+              <div className={classNames('ml-2', circle)}>
+                <Row className='align-content-center'>
+                  <Col xs='12'>
+                    <i className={classNames(icon, 'fa fa-rocket')}></i>
+                  </Col>
+                  <Col xs='12'>
+                    <small>fast</small>
+                  </Col>
+                </Row>
+              </div>
+              <div className={classNames('ml-2', circle)}>
+                <Row className='align-content-center'>
+                  <Col xs='12'>
+                    <Icon src={SwapIcon} className={classNames(icon, swapIcon)} height={20} width={20} color='#fff' />
+                  </Col>
+                  <Col xs='12'>
+                    <small>swap</small>
+                  </Col>
+                </Row>
+              </div>
+            </div>
           </div>
         </Col>
-        <Col sm='6' className='p-0'>
+        <Col className='p-0' sm='2'>
         </Col>
       </Row>
     </div>
