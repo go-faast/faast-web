26955ec1a95aca2e9f5a3586b454a073c5f0b801
Matus Dubrava <dankonino@gmail.com>
2019-08-07 22:26:27 +0100

    more navigation tests

--- test/e2e/navigation.js
+++ test/e2e/navigation.js
@@ -3,13 +3,16 @@ import { Selector } from 'testcafe'
 export default (config) => {
   fixture `Navigation`
     .page(config.baseUrl)
+  
+    const LINK =  (x) =>  Selector('a').withAttribute('href', x).exists
+    const HREF =  (x) =>  Selector('a').withAttribute('href', x)  
 
-  // test('Home page', async t => {
-  //   const header = Selector('.hero-title').find('.mb-4')
-  //   await t
-  //     .expect(header.innerText)
-  //     .eql('Don’t Get Goxxed. Own Your Crypto.')
-  // })
+  test('Home page', async t => {
+    const header = Selector('.hero-title').find('.mb-4')
+    await t
+      .expect(header.innerText)
+      .eql('Don’t Get Goxxed. Own Your Crypto.')
+  })
 
   test('Navigate to Market Maker', async t => {
     await t
@@ -26,29 +29,95 @@ export default (config) => {
   })
 
   test('See the home page urls', async t => {
-    const firstswap = Selector('a').withAttribute('href', '/app/swap?from=BTC&to=ETH').exists    
-    const walletconnect = Selector('a').withAttribute('href', '/app/connect').exists
-    const assests = Selector('a').withAttribute('href', '/assets').exists
-    const startTrading = Selector('a').withAttribute('href', '/app').exists
-    const trezor = Selector('a').withAttribute('href', '/wallets/trezor').exists
-    const ledger = Selector('a').withAttribute('href', '/wallets/ledger-wallet').exists
-    const metamask = Selector('a').withAttribute('href', '/wallets/metamask').exists
-    const mist = Selector('a').withAttribute('href', '/wallets/mist-browser').exists
-    const trustwallet = Selector('a').withAttribute('href', '/wallets/trust-wallet').exists
-    const coinbase = Selector('a').withAttribute('href', '/wallets/coinbase-wallet').exists
-    const status = Selector('a').withAttribute('href', '/wallets/status').exists
+    await t
+    .expect(LINK('/app/swap?from=BTC&to=ETH')).ok()
+    .expect(LINK('/app/connect')).ok()
+    .expect(LINK('/wallets/trezor')).ok()
+    .expect(LINK('/wallets/ledger-wallet')).ok()
+    .expect(LINK('/wallets/metamask')).ok()
+    .expect(LINK('/wallets/mist-browser')).ok()
+    .expect(LINK('/wallets/trust-wallet')).ok()
+    .expect(LINK('/wallets/coinbase-wallet')).ok()
+    .expect(LINK('/wallets/status')).ok()    
+  })
+
+  test('Bottom menu links', async t => {
 
     await t
-      .expect(firstswap).ok()    
-      .expect(walletconnect).ok()
-      .expect(assests).ok()
-      .expect(startTrading).ok()
-      .expect(trezor).ok()
-      .expect(ledger).ok()
-      .expect(metamask).ok()
-      .expect(mist).ok()
-      .expect(trustwallet).ok()
-      .expect(coinbase).ok()
-      .expect(status).ok()
+      .expect(LINK('/what-are-smart-contracts')).ok()
+      .expect(LINK('/affiliates')).ok()
+      .expect(LINK('/app')).ok()
+      .expect(LINK('/app/swap')).ok()
+      .expect(LINK('/blog')).ok()
+      .expect(LINK('/app/assets/trending')).ok()
+      .expect(LINK('/app/assets/watchlist')).ok()
+      .expect(LINK('/what-is-an-ico')).ok()
+      .expect(LINK('/what-are-smart-contracts')).ok() 
+      .expect(LINK('/what-is-the-difference-between-ico-ipo-ito')).ok() 
+      .expect(LINK('/what-is-ethereum')).ok() 
+      .expect(LINK('/what-is-a-dao')).ok() 
+      .expect(LINK('/how-to-buy-ethereum')).ok() 
+      .expect(LINK('https://api.faa.st/')).ok()
+      .expect(LINK('/static/faast-press-kit.zip')).ok()
   })
+
+  test('Thumb of page', async t => {
+    await t
+      .expect(LINK('mailto:support@faa.st')).ok()
+      .expect(LINK('/terms')).ok()
+      .expect(LINK('/privacy')).ok()
+      .expect(LINK('https://github.com/go-faast')).ok()
+      .expect(LINK('https://www.facebook.com/Faast-237787136707810')).ok()
+      .expect(LINK('https://twitter.com/gofaast')).ok()
+      .expect(LINK('https://slack.faa.st/')).ok()
+      .expect(LINK('https://www.reddit.com/r/gofaast/')).ok()
+  })
+
+  test('Goes to bunch of subpages', async t => {
+    const affiliateid = Selector('input').withAttribute('placeholder', 'Affiliate Id')
+    const affil = HREF('/affiliates')
+    const supported = HREF('/assets')
+
+    await t
+      // .click(affil)
+      // .expect(affiliateid).ok()
+      .click(supported)
+      .expect(LINK('/app/swap?to=BTC')).ok()
+  })
+
+  test('Goes to trending', async t => {
+    const tradeable = Selector('#form-tradeableForm-requiredCheckbox').exists
+    await t
+      .click(HREF('/app/assets/trending'))
+      .expect(tradeable).ok()
+  })
+
+  test('Goes to wallets info', async t => {
+    await t
+      .click(HREF('/wallets/trezor'))
+      .expect(LINK('https://trezor.io/')).ok()
+
+      .click(HREF('/wallets/ledger-wallet'))
+      .expect(LINK('https://www.ledger.com/')).ok()
+
+      .click(HREF('/wallets/metamask'))
+      .expect(LINK('https://metamask.io/')).ok()
+
+      .click(HREF('/wallets/mist-browser'))
+      .expect(HREF('https://github.com/ethereum/mist')).ok()
+
+      .click(HREF('/wallets/trust-wallet'))
+      .expect(LINK('https://trustwalletapp.com/')).ok()
+
+      .click(HREF('/wallets/coinbase-wallet'))
+      .expect(LINK('https://wallet.coinbase.com/')).ok()
+
+      .click(HREF('/wallets/status'))
+      .expect(LINK('https://status.im/')).ok()
+  })
+
+
+
+
+
 }
