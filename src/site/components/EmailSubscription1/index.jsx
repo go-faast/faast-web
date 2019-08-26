8e3222e1c61adb37f53900a6b76a59f07a1a6242
dwolfgram <daniel.j.wolfgram@gmail.com>
2019-08-26 16:28:45 +0200

    email widget

--- src/site/components/EmailSubscription1/index.jsx
+++ src/site/components/EmailSubscription1/index.jsx
@@ -1,60 +1,26 @@
 import React from 'react'
-import * as validator from 'Utilities/validator'
-import { compose, setDisplayName, withHandlers } from 'recompose'
-import { Form, Button } from 'reactstrap'
-import { reduxForm } from 'redux-form'
-import ReduxFormField from 'Components/ReduxFormField'
+import { compose, setDisplayName, } from 'recompose'
 
-import style from './style.scss'
-
-import classNames from 'class-names'
-
-const FORM_NAME = 'email_form'
-//const getFormValue = formValueSelector(FORM_NAME)
-
-const EmailSub = ({ handleSubmit, translations: { static: { email: t } } }) => {
+const EmailSub = ({ translations: { static: { email: t } } }) => {
   return (
-    <Form onSubmit={handleSubmit}>
-      <ReduxFormField
-        className='text-center'
-        label={<p style={{ color: '#EFEFEF', fontSize: 18 }}>{t.cta}</p>}
-        labelClass='w-100 text-center'
-        name='email'
-        inputClass={classNames('flat mx-auto', style.input)}
-        type='text'
-        placeholder='Email Address'
-        inputGroupClass='flat mx-auto position-relative'
-        inputGroupStyle={{ maxWidth: 495 }}
-        addonAppend={() => (
-          <Button 
-            className={classNames('flat position-absolute text-center p-0')} 
-            style={{ top: 0, right: 0, width: 120,
-              height: '100%', backgroundColor: '#D5ECE8',
-              border: 'none', color: '#16AD94',
-              fontSize: 14, fontWeight: 600, zIndex: 99999 }}
-            color='primary' 
-            type='submit' 
-          >
-            {t.subscribe}
-          </Button>
-        )
-        }
-      />
-    </Form>
+    <div className='text-center'>
+      <p style={{ color: '#EFEFEF', fontSize: 18 }}>{t.cta}</p>
+      <section className='container-fluid signup-wrapper'>
+        <iframe 
+          className='position-relative'
+          style={{ zIndex: 9999, width: '100%', maxWidth: 500 }}
+          frameBorder='0' 
+          scrolling='no' 
+          marginHeight='0' 
+          marginWidth='0' 
+          src='https://app.mailjet.com/widget/iframe/3lll/iI7'
+        >
+        </iframe>
+      </section>
+    </div>
   )
 }
 
 export default compose(
   setDisplayName('EmailSub'),
-  withHandlers({
-    validateRequired: () => validator.all(
-      validator.required('This field is required.'),
-    ),
-    onSubmit: () => ({  email }) => {
-      console.log(email)
-    }
-  }),
-  reduxForm({
-    form: FORM_NAME,
-  }),
 )((EmailSub))
