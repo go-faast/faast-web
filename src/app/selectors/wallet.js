fa47357f0898a5e75729979fd65b34b14862f8ed
Dylan Seago <dylan@bitaccess.co>
2019-08-28 11:30:07 -0400

    Remove Services/Wallet import from wallet selectors

--- src/app/selectors/wallet.js
+++ src/app/selectors/wallet.js
@@ -5,7 +5,6 @@ import { ZERO, toUnit, toPercentage } from 'Utilities/convert'
 import { fixPercentageRounding, reduceByKey, mapValues } from 'Utilities/helpers'
 import { createItemSelector, selectItemId, fieldSelector } from 'Utilities/selector'
 
-import { MultiWallet } from 'Services/Wallet'
 import { getAllAssets, areAssetPricesLoaded, getAssetPricesError } from './asset'
 
 const getWalletState = ({ wallet }) => wallet
@@ -17,7 +16,7 @@ const doGetWallet = (walletState, id) => {
   }
   const nestedWallets = wallet.nestedWalletIds.map((nestedWalletId) => doGetWallet(walletState, nestedWalletId)).filter(Boolean)
   let { balances, balancesLoaded, balancesUpdating, balancesError, supportedAssets, unsendableAssets } = wallet
-  if (wallet.type.includes(MultiWallet.type)) {
+  if (wallet.type.includes('MultiWallet')) {
     if (nestedWallets.length) {
       balances = reduceByKey(nestedWallets.map((w) => w.balances), (x, y) => x.plus(y), ZERO)
       balancesLoaded = nestedWallets.every((w) => w.balancesLoaded)
