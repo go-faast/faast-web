66668f516cd0147bcffd0cf4246ed9eaddf638dd
dwolfgram <daniel.j.wolfgram@gmail.com>
2020-07-29 17:03:54 +0200

    uglify change

--- etc/webpack.config.base.js
+++ etc/webpack.config.base.js
@@ -153,11 +153,11 @@ module.exports = function (stage, outputPathPrefix = '') {
       }),
       new HappyPack({
         id: 'css1',
-        loaders: cssLoader({ modules: false })
+        loaders: [...cssLoader({ modules: false })]
       }),
       new HappyPack({
         id: 'css2',
-        loaders: cssLoader(),
+        loaders: [...cssLoader()],
       }),
       new HappyPack({
         id: 'img',
@@ -175,38 +175,6 @@ module.exports = function (stage, outputPathPrefix = '') {
         new UglifyJsPlugin({
           sourceMap: false,
           cache: true,
-          compress: {
-            arrows: false,
-            booleans: false,
-            cascade: false,
-            collapse_vars: false,
-            comparisons: false,
-            computed_props: false,
-            hoist_funs: false,
-            hoist_props: false,
-            hoist_vars: false,
-            if_return: false,
-            inline: false,
-            join_vars: false,
-            keep_infinity: true,
-            loops: false,
-            negate_iife: false,
-            properties: false,
-            reduce_funcs: false,
-            reduce_vars: false,
-            sequences: false,
-            side_effects: false,
-            switches: false,
-            top_retain: false,
-            toplevel: false,
-            typeofs: false,
-            unused: false,
-            // Switch off all types of compression except those needed to convince
-            // react-devtools that we're using a production build
-            conditionals: true,
-            dead_code: true,
-            evaluate: true,
-          },
           parallel: true,
           uglifyOptions: {
             mangle: {
